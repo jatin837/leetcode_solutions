8aad24c171f43d0b3fcc9a8e9c8fa85eedc7b86f
japan <pandeyjatin47@gmail.com>
2022-04-16 10:05:08 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (23.87%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int l=0, r=nums.size()-1;
+      while(l<=r) {
+        int mid = ((r-l)>>1)+l;
+        if(nums[mid]==target)
+          return mid;
+        else if(nums[mid]<target)
+          l=mid+1;
+        else
+          r=mid-1;
+        }
+        return l;
+    }
+};
