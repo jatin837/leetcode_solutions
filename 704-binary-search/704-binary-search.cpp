72498c0e218a8a68c0436b5a026478a029e14b30
japanzzy <pandeyjatin47@gmail.com>
2022-04-17 11:55:39 +0530

    Time: 42 ms (71.56%), Space: 27.5 MB (63.66%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -4,10 +4,12 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
       auto it = lower_bound(nums.begin(), nums.end(), target);
-      if(it == nums.end())
+      if(it == nums.end()){
         return -1;
-      if (target != *it)
+      }
+      if (target != *it){
         return -1;
+      }
       return it - nums.begin();
     }
   
