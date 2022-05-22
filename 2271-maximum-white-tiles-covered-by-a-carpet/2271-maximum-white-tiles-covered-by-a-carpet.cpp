8fdf1d7eec55580ab59e3d4e07fcca35d13c2455
japanzzy <pandeyjatin47@gmail.com>
2022-05-22 09:23:18 +0530

    Time: 404 ms (83.68%), Space: 69.9 MB (70.36%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -23,18 +23,18 @@
 
 class Solution {
 public:
-    int maximumWhiteTiles(vector<vector<int>>& t, int len) {
-        sort(t.begin(),t.end());
-        int n=t.size(),s=0,e=0,ans=0,area=0;
+    int maximumWhiteTiles(vector<vector<int>>& tiles, int len) {
+        sort(tiles.begin(),tiles.end());
+        int n=tiles.size(),s=0,e=0,ans=0,area=0;
         for(;e<n && s<n;){
-            if(t[s][0]+len>t[e][1]){
-                area+=t[e][1]-t[e][0]+1;
+            if(tiles[e][1] - tiles[s][0] < len){
+                area+=tiles[e][1]-tiles[e][0]+1;
                 ans=max(ans,area);   
                 ++e;
             }
             else{
-                ans=max(ans,area+max(0,t[s][0]+len-t[e][0]));
-                area-=(t[s][1]-t[s][0]+1);
+                ans=max(ans,area+max(0,tiles[s][0]+len-tiles[e][0]));
+                area-=(tiles[s][1]-tiles[s][0]+1);
                 ++s;
             }
         }
