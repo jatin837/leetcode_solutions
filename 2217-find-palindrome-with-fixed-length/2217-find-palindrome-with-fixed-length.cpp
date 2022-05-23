dd6dd5694c09f3ba9e2a8c2684df409d4bff9318
japanzzy <pandeyjatin47@gmail.com>
2022-05-23 20:26:34 +0530

    Time: 183 ms (80.46%), Space: 53.8 MB (74.46%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -1,27 +1,24 @@
 #define ll unsigned long long int
 class Solution {
 public:
-  vector<long long> kthPalindrome(vector<int>& q, int n) {
-    long long h=0;
+  vector<long long> kthPalindrome(vector<int>& qs, int n) {
+    long long max_possible=0;
     int j = n;
     while(j--)
-      h = h*10 + 9;
-    long long k;
-    if(n%2==0)
-      k = n/2 - 1;
-    else
-      k = n/2;
+      max_possible = max_possible*10 + 9;
+    
+    long long k = (n-1)>>1;
     long long m = 1;
+    
     while(k--)
       m = m*10;
     
     vector<long long> res;
 
-    for(int i=0; i<q.size(); i++){
+    for(auto q:qs){
       ll num = m;
-      num = num+(q[i]-1);
+      num = num+(q-1);
       long long b;
-
       if(n%2==1)
         b = num/10;
       else
@@ -30,7 +27,7 @@ public:
         num = num*10 + b%10;
         b = b/10;
       }
-      if(num>h)
+      if(num>max_possible)
         res.push_back(-1);
       else
         res.push_back(num);
