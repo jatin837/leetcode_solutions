1ddbbe5684245de22fee08068b9baf4234cf53a0
japanzzy <pandeyjatin47@gmail.com>
2022-05-26 10:01:58 +0530

    Time: 5 ms (80.56%), Space: 6.8 MB (92.18%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+      int sind[129] = {0}, tind[129] = {0};
+      
+      for(int i = 0; i < s.size(); i++){
+        if(sind[s[i]] != tind[t[i]]) return false;
+        sind[s[i]] = tind[t[i]] = i+1;
+      }
+      
+      return true;
+    }
+};
