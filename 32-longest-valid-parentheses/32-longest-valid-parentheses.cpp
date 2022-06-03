5dae09148edf84e2297822d35c6b53cf09b8487c
japanyzz <pandeyjatin47@gmail.com>
2022-06-04 00:55:05 +0530

    Time: 8 ms (38.45%), Space: 7.3 MB (38.44%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+      int ans = 0;
+      vector<int>dp(s.length(), 0);
+      for(int i=1; i<dp.size(); i++){
+        if(s[i] == ')'){
+          if(s[i-1] == '(')
+            dp[i] = (i>=2 ? dp[i-2]:0)+2;
+          else if(
+            i - dp[i-1] > 0 and s[i-dp[i-1]-1] == '('
+          )
+            dp[i] = dp[i-1] + ((i-dp[i-1]) >= 2? dp[i-dp[i-1]-2]:0)+2;  
+        }
+        ans = max(ans, dp[i]);
+      }
+      return ans;
+    }
+};
