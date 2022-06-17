c7af57a644d895a53176740d51f5ef2630374502
japanyzz <pandeyjatin47@gmail.com>
2022-06-17 16:01:03 +0530

    Time: 10 ms (84.86%), Space: 21.3 MB (55.06%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,19 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
+  solve function takes a tree node pointer and returns the an array of size three
+  =====================================================================
+  arr[0] => minimum number of cameras from bottom of tree upto this node such that neither this node is covered with camera nor monitored but it's subtree and monitored
+  arr[1] => minimum number of cameras from bottom of tree upto this node such that this node is not covered with camera, but this as well as it's subtrees are monitored completely
+  arr[2] => minimum number of cameras from bottom of tree upto this node such that this node is covered with camera and it's subtrees are completely monitored
+  
+  The Base Case
+  =============
+  if i have an empty tree, then minimum number of cameras i need to monitor left and right subtrees and not place camera at root node is 0
+  the above statement covers arr[0] and arr[1] for this base case
+  not, if i place a camera here at the root, this is not possible, because there is no tree at all, so arr[2] should be infinite
+  the answer of this problems is minimum of arr[2] and arr[3] for root node
+  
+  
+  
+  This was a pretty clever problem, and leetcode solution was very impressive.
+  It was in java & python, so implemented it in c++.
+*/
 class Solution {
 public:
     array<int, 3> solve(TreeNode* root){
       if(root == nullptr)
-        return {0, 0, 99999};
+        return {0, 0, 1001};
       array<int, 3> left = solve(root->left);
       array<int, 3> right = solve(root->right);
       int mL12 = min(left[1], left[2]);
