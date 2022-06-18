d8a2215f65cac0924412629f07b56e7994c0a3a6
japanyzz <pandeyjatin47@gmail.com>
2022-06-18 09:43:37 +0530

    Time: 104 ms (78.38%), Space: 22 MB (76.48%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int minimumDeletions(string s) {
       int len = s.length();
-      vector<int>dp(len+1);
       int bcnt = 0;
-      if(s[0] == 'b')
-        bcnt++;
-      for(int i=1; i<len; i++){
+      int dp1=0;int dp2=0;
+      for(int i=0; i<len; i++){
         if(s[i] == 'a'){
-          // either keep this a or remove this a
-          dp[i] = min(dp[i-1] + 1, bcnt);
+          dp2=min(dp1+1, bcnt);
         }else{
           bcnt++;
-          dp[i] = dp[i-1];
+          dp2=dp1;
         }
+        dp1=dp2;
       }
-      return dp[len-1];
+      return dp2;
     }
 };
