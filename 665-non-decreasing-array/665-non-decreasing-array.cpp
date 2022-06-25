0f7ac654e9225f567fd3f76a021024083c314733
japanyzz <pandeyjatin47@gmail.com>
2022-06-25 10:40:18 +0530

    Time: 30 ms (85.25%), Space: 27.1 MB (12.14%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+      int len = nums.size();
+      bool changed = false;
+      if(nums.size() == 1)
+        return true;
+      int cnt = 0;
+      if(nums[0] > nums[1]){
+        nums[0] = nums[1];
+        cnt++;
+      }
+      for(int i=1; i<len-1; i++){
+        if(nums[i] <= nums[i+1]) 
+          continue;
+        if(nums[i-1] <= nums[i+1])
+          nums[i] = nums[i+1];
+        else
+          nums[i+1] = nums[i];
+        cnt++;
+      }
+      return cnt<=1;
+    }
+};
