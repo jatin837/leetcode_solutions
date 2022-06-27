dfc96cca93cf0a4d995c146d8e215a072828c4d2
japanyzz <pandeyjatin47@gmail.com>
2022-06-27 14:37:00 +0530

    Time: 486 ms (60.37%), Space: 28.2 MB (35.37%) - LeetHub

---
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long sellingWood(int m, int n, vector<vector<int>>& prices) {
+     vector<vector<long long>>dp(m+1, vector<long long>(n+1, 0));   
+     for(vector<int> price:prices)
+       dp[price[0]][price[1]] = price[2];
+      for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+          for(int k=1; k<=i/2; k++)
+            dp[i][j] = max(dp[i][j], dp[k][j] + dp[i-k][j]);
+          for(int k=1; k<=j/2; k++)
+            dp[i][j] = max(dp[i][j], dp[i][k] + dp[i][j-k]);
+        }
+      }
+      return dp[m][n];
+    }
+};
