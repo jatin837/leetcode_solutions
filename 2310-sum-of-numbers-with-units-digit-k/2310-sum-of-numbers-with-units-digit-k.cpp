e04ccc363d1ab1f5d1f4ed4919ec091f04a2ab3b
japanyzz <pandeyjatin47@gmail.com>
2022-06-27 17:47:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.85%) - LeetHub

---
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumNumbers(int num, int k) {
+      if (num == 0)
+        return 0;
+      for(int i=1; i<=10; i++)
+        if(i*k%10 == num%10 and i*k <= num)
+          return i;
+      return -1;
+    }
+};
