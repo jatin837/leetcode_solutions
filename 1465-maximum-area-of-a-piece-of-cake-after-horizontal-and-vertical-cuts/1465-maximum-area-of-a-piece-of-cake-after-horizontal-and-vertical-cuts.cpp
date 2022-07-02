44aecca3fb14be13c9b51bf6d98a959f46965287
jatin.py <pandeyjatin47@gmail.com>
2022-07-02 10:13:55 +0530

    Time: 122 ms (27.36%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -3,6 +3,8 @@ public:
     int maxArea(int hh, int ww, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
       sort(horizontalCuts.begin(), horizontalCuts.end());
       sort(verticalCuts.begin(), verticalCuts.end());
+      horizontalCuts.push_back(hh);
+      verticalCuts.push_back(ww);
       long long maxH=horizontalCuts[0];
       long long maxW=verticalCuts[0];
       for(int i=1; i<horizontalCuts.size(); i++){
@@ -13,8 +15,6 @@ public:
         long long w = verticalCuts[i] - verticalCuts[i-1];
         maxW = max(w, maxW);
       }
-      maxH = max(maxH, (long long)(hh-horizontalCuts.back()));
-      maxW = max(maxW, (long long)(ww-verticalCuts.back()));
       long long mod = 1e9+7;
       return maxW*maxH%mod;
     }
