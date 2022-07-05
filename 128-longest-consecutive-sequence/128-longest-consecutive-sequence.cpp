0d61296e044092554bf2ca0c9780d5ed5586a2db
jatin.py <pandeyjatin47@gmail.com>
2022-07-05 09:58:51 +0530

    Time: 161 ms (74.70%), Space: 45.7 MB (73.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
       unordered_set<int>num_set(begin(nums), end(nums));
       int ans = 0;
       for(int num:num_set){
-        if(num_set.count(num-1) == 0){
+        if(!num_set.count(num-1)){
           int currNum = num;
           int currStreak = 1;
           while(num_set.count(currNum+1)){
