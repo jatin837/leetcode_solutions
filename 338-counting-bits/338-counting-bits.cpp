f0286ebef68533412f2ef27e1696bc98b73e1ae5
jatin.py <pandeyjatin47@gmail.com>
2022-07-06 10:36:22 +0530

    Time: 3 ms (97.15%), Space: 7.8 MB (72.90%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,10 +3,8 @@ public:
     vector<int> countBits(int n) {
       vector<int>dp(n+1);
       dp[0] = 0;
-      for(int i=1; i<=n; i++){
+      for(int i=1; i<=n; i++)
         dp[i] = dp[i>>1] + int(i&1);
-        cout<<int(i&1)<<'\n';
-      }
       return dp;
     }
 };
