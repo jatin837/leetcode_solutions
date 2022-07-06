324abf5b44356c771d0ba1e19f58a60f439b137c
jatin.py <pandeyjatin47@gmail.com>
2022-07-06 10:49:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.04%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -10,10 +10,7 @@ public:
       dp[1] = 1;
       int ans = 1;
       for(int i=2; i<=n; i++){
-        if(i%2==0)
-          dp[i] = dp[i>>1];
-        else
-          dp[i] = dp[i>>1] + dp[(i>>1) + 1];
+        dp[i] = dp[i>>1] + int(i&1)*dp[(i>>1) + 1];
         ans = max(ans, dp[i]);
       }
       return ans;
