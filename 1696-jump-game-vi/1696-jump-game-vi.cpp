915848ab3bf3083fdec435eacb03fd2d01462ade
jatin.py <pandeyjatin47@gmail.com>
2022-07-09 09:39:12 +0530

    Time: 1238 ms (5.05%), Space: 90.5 MB (49.00%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+      priority_queue<pair<int, int>> pq;
+      pq.push({nums[0], 0});
+      for(int i=1; i<nums.size(); i++){
+        while(pq.top().second <= i-k-1)
+          pq.pop();
+        nums[i] += pq.top().first;
+        pq.push({nums[i], i});
+      }
+      for(int n:nums)
+        cout<<n<<' ';
+      return nums.back();
+    }
+};
