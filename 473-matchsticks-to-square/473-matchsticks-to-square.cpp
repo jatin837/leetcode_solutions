9511ce9e47f0b0d304a5c3705cf34f92f12532a1
jatin.py <pandeyjatin47@gmail.com>
2022-07-12 11:27:12 +0530

    Time: 343 ms (53.53%), Space: 10 MB (66.64%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(vector<int> &matchsticks, int idx, int sideLen, vector<int>& sq){
+      if(idx == matchsticks.size())
+        return sq[0] == sq[1] and sq[1] == sq[2] and sq[3] == sq[2];
+      int currMatchStick = matchsticks[idx];
+      for(int i=0; i<4; i++){
+        if(currMatchStick + sq[i] <= sideLen){
+          sq[i]+=currMatchStick;
+          if(dfs(matchsticks, idx+1, sideLen, sq))
+            return true;
+          sq[i]-=currMatchStick;
+        }
+      }
+      return false;
+    }
+    static bool cmp(int &l, int &r){
+      return l>r;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+      int sum = accumulate(begin(matchsticks), end(matchsticks), 0);
+      if(sum%4!=0)
+        return false;
+      int sideLen = sum>>2;
+      vector<int>sq(4, 0);
+      sort(begin(matchsticks), end(matchsticks), cmp);
+      return dfs(matchsticks, 0, sideLen, sq);
+    }
+};
+
