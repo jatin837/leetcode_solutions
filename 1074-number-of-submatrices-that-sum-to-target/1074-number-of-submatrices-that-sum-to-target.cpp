4eade3a95e8b9a65ab36a3b51dd535791c300926
jatin.py <pandeyjatin47@gmail.com>
2022-07-18 14:31:09 +0530

    Time: 1131 ms (55.82%), Space: 253.5 MB (25.88%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -8,12 +8,14 @@ public:
         for(int j=1; j<cl; j++)
           matrix[i][j] += matrix[i][j-1];
       for(int i=0; i<cl; i++){
-        for(int j=i; j<cl; j++){
+        for(int j=0; j<=i; j++){
           unordered_map<int, int> preCnt;
           preCnt[0] = 1;
           int sum = 0;
           for(int k=0; k<rl; k++){
-            sum += matrix[k][j] - (i>0?matrix[k][i-1]:0);
+            sum += matrix[k][i];
+            if(j != 0)
+              sum -= matrix[k][j-1];
             cnt += preCnt[sum - target];
             preCnt[sum]++;
           }
