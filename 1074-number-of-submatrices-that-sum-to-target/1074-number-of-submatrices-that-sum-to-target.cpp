4fe096aeec413a85e402d81f532ba43b8ff64104
jatin.py <pandeyjatin47@gmail.com>
2022-07-18 14:57:48 +0530

    Time: 1830 ms (12.17%), Space: 253.6 MB (22.49%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
+    int subarraySum(vector<int>& nums, int k) {
+      unordered_map<int, int>preCount;
+      preCount[0] = 1;
+      int sum = 0;
+      int cnt = 0;
+      for(int n:nums){
+        sum += n;
+        cnt += preCount[sum-k];
+        preCount[sum]++;
+      }
+      return cnt;
+    }
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
       int rl = matrix.size();
       int cl = matrix[0].size();
+      vector<int>compressedArr(rl);
       int cnt = 0;
       for(int i=0; i<rl; i++)
         for(int j=1; j<cl; j++)
           matrix[i][j] += matrix[i][j-1];
       for(int i=0; i<cl; i++){
         for(int j=0; j<=i; j++){
-          unordered_map<int, int> preCnt;
-          preCnt[0] = 1;
-          int sum = 0;
           for(int k=0; k<rl; k++){
-            sum += matrix[k][i];
+            compressedArr[k] = matrix[k][i];
             if(j != 0)
-              sum -= matrix[k][j-1];
-            cnt += preCnt[sum - target];
-            preCnt[sum]++;
+              compressedArr[k] -= matrix[k][j-1];
           }
+          cnt += subarraySum(compressedArr, target);
         }
       }
       return cnt;
