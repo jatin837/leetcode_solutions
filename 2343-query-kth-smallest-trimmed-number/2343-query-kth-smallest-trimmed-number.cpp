6cf0de43d3e96291b6920806444415d7c7537070
jatin.py <pandeyjatin47@gmail.com>
2022-07-20 17:46:35 +0530

    Time: 1676 ms (11.11%), Space: 372.2 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -5,8 +5,9 @@ public:
         int n = nums.size();
         for(auto &q:queries) {
             vector<pair<string,int>> pq;
+            int start = nums[0].size()-q[1];
             for(int i=0;i<n; i++)
-                pq.push_back({nums[i].substr(nums[i].size()-q[1]), i});
+                pq.push_back({nums[i].substr(start), i});
             sort(pq.begin(), pq.end());
             res.push_back(pq[q[0]-1].second);
         }
