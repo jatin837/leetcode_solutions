5f480423ccde9be1caabb68603a2b93bfd48b2e6
jatin.py <pandeyjatin47@gmail.com>
2022-07-22 15:01:02 +0530

    Time: 15 ms (14.89%), Space: 10.2 MB (94.54%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,42 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-      
-      ListNode *A = new ListNode(-1), *B = new ListNode(-1), *pA = A, *pB = B;
+      ListNode* A = new ListNode(-1);
+      ListNode* B = new ListNode(-1);
+      ListNode* pA =A;
+      ListNode* pB = B;
       while(head){
-        ListNode* curr = new ListNode(head->val);
-        if(curr->val >= x){
-          pA->next = curr;
-          pA = curr;
+        if(head->val < x){
+          pA->next = head;
+          pA = head;
         }else{
-          pB->next = curr;
-          pB = curr;
+          pB->next = head;
+          pB = head;
         }
         head = head->next;
       }
-////  ListNode* ans = new ListNode(0);
-////  ListNode* prev = ans;
-////  for(int i=0; i<a.size(); i++){
-////    ListNode* curr = new ListNode(a[i]);
-////    prev->next = curr;
-////    prev = curr;
-////  }
-////  for(int i=0; i<b.size(); i++){
-////    ListNode* curr = new ListNode(b[i]);
-////    prev->next = curr;
-////    prev = curr;
-////  }
-      pB->next = A->next;
-      return B->next;
+      pB->next = nullptr;
+      pA->next = B->next;
+      return A->next;
     }
 };
