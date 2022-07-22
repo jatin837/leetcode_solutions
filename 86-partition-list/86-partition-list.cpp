aefd236431d9765b21fe4553addc5293944738c9
jatin.py <pandeyjatin47@gmail.com>
2022-07-22 15:02:29 +0530

    Time: 6 ms (77.09%), Space: 10.2 MB (73.43%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-      ListNode* A = new ListNode(-1);
-      ListNode* B = new ListNode(-1);
-      ListNode* pA =A;
-      ListNode* pB = B;
+      ListNode *A = new ListNode(-1), *B = new ListNode(-1);
+      ListNode *pA =A, *pB = B;
       while(head){
         if(head->val < x){
           pA->next = head;
