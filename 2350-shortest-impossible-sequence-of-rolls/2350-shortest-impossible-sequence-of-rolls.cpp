393ba06373adc9578dad7bbd9dd960faefdbdeb0
jatin.py <pandeyjatin47@gmail.com>
2022-07-24 11:19:16 +0530

    Time: 432 ms (100.00%), Space: 108.4 MB (100.00%) - LeetHub

---
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int shortestSequence(vector<int>& A, int k) {
+    int res = 1;
+    unordered_set<int> s;
+    for (int& a : A) {
+      s.insert(a);
+      if (s.size() == k) {
+        res++;
+        s.clear();
+      }
+    }
+    return res;
+  }
+};
