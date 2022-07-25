af7a7647ef158a31d394904943afd185188f3231
jatin.py <pandeyjatin47@gmail.com>
2022-07-25 09:17:25 +0530

    Time: 9 ms (75.28%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
       vector<int> ans(2, -1);
       if(nums.size() == 0)
-        return ans;
+        return {-1, -1};
       int b = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
       int e = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-      if(b == nums.size())
-        return ans;
-      if(nums[b] != target)
-        return ans;
-      ans[0] = b;
-      ans[1] = e-1;
-      return ans;
+      if(b == nums.size() or nums[b] != target)
+        return {-1, -1};
+
+      return {b, e-1};
     }
 };
