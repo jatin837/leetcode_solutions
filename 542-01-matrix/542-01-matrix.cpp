7f10b3a4e59edc1e4924630cf73ee6fcc5676072
jatin.py <pandeyjatin47@gmail.com>
2022-08-01 12:50:57 +0530

    Time: 189 ms (17.01%), Space: 45.4 MB (11.34%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -2,30 +2,24 @@
 #define P pair<int, int>
 class Solution {
 public:
-    vector<pair<int, int>> dd = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    vector<vector<int>> updateMatrix(vector<vector<int>>& M){
-      queue<P>q;
-      for(int i=0; i<M.size(); i++)
-        for(int j=0; j<M[0].size(); j++){
-          if(!M[i][j])
-            q.push({i, j});
-          M[i][j]=-1;
-        }
-      int curr = 0;
-      while(!q.empty()){
-        queue<P>tmp;
-        while(!q.empty()){
-          auto [x, y] = q.front();
-          q.pop();
-          if(x < 0 || y < 0 || x >= M.size() || y >= M[0].size() || M[x][y] != -1)
-            continue;
-          M[x][y] = curr;
-          for(auto [dx, dy]:dd)
-            tmp.push({x+dx, y+dy});
-        }
-        curr++;
-        q = tmp;
-      }
-      return M;
+vector<pair<int, int>> dd = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+vector<vector<int>> updateMatrix(vector<vector<int>>& M){
+  queue<array<int, 3>>q;
+  for(int i=0; i<M.size(); i++)
+    for(int j=0; j<M[0].size(); j++){
+      if(!M[i][j])
+        q.push({i, j, 0});
+      M[i][j]=-1;
     }
+  while(!q.empty()){
+    auto [x, y, curr] = q.front();
+    q.pop();
+    if(x < 0 || y < 0 || x >= M.size() || y >= M[0].size() || M[x][y] != -1)
+      continue;
+    M[x][y] = curr;
+    for(auto [dx, dy]:dd)
+      q.push({x+dx, y+dy, curr+1});
+  }
+  return M;
+}
 };
