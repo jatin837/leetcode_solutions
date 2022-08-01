60f998a845405f53f7f04dd9b60e3b6266aeefd5
jatin.py <pandeyjatin47@gmail.com>
2022-08-01 12:44:39 +0530

    Time: 244 ms (11.92%), Space: 64.6 MB (6.58%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -4,27 +4,28 @@ class Solution {
 public:
     vector<pair<int, int>> dd = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& M){
-      vector<vector<int>>dist = vector(M.size(), vector<int>(M[0].size(), -1));
       queue<P>q;
       for(int i=0; i<M.size(); i++)
-        for(int j=0; j<M[0].size(); j++)
+        for(int j=0; j<M[0].size(); j++){
           if(!M[i][j])
             q.push({i, j});
+          M[i][j]=-1;
+        }
       int curr = 0;
       while(!q.empty()){
         queue<P>tmp;
         while(!q.empty()){
           auto [x, y] = q.front();
           q.pop();
-          if(x < 0 || y < 0 || x >= M.size() || y >= M[0].size() || dist[x][y] != -1)
+          if(x < 0 || y < 0 || x >= M.size() || y >= M[0].size() || M[x][y] != -1)
             continue;
-          dist[x][y] = curr;
+          M[x][y] = curr;
           for(auto [dx, dy]:dd)
             tmp.push({x+dx, y+dy});
         }
         curr++;
         q = tmp;
       }
-      return dist;
+      return M;
     }
 };
