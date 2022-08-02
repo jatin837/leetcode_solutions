fb6ffadb04ac4a1d875d7b4db8d87d3e3cb11e85
jatin.py <pandeyjatin47@gmail.com>
2022-08-02 12:51:30 +0530

    Time: 246 ms (5.30%), Space: 47.2 MB (7.68%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-//  static bool cmp(int& a, int& b){
-//    return a > b;
-//  }
-//  int findKthLargest(vector<int>& nums, int k) {
-//    sort(begin(nums), end(nums), cmp);
-//    return nums[k-1];
-//  }
     int findKthLargest(vector<int>& nums, int k){
       priority_queue<int> pq;
       for(int num:nums){
-        pq.push(num);
-        if(pq.size() > nums.size()-k+1)
+        pq.push(-num);
+        if(pq.size() > k)
           pq.pop();
       }
-      return pq.top();
+      return -pq.top();
     }
 };
