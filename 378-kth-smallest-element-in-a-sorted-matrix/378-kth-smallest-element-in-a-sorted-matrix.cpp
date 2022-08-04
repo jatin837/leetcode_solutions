dd5e744bdf97a424d485c7fbd6b7d40b9c86cae6
jatin.py <pandeyjatin47@gmail.com>
2022-08-04 14:10:31 +0530

    Time: 38 ms (83.41%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>&M , int k) {
-      int b = *(M.begin()->begin()), e = M.back().back();
+      int b = M[0][0], e = M.back().back();
       int ans = -1;
       while(b<=e){
         long long m = (b+e)>>1;
         int smlCnt = 0;
         for(int i=0; i<M.size(); i++)
           smlCnt += upper_bound(M[i].begin(), M[i].end(), m)-M[i].begin();
-        if(smlCnt >= k){
-          ans = m;
-          e = m-1;
-        }
-        else
-          b = m+1;
+          if (smlCnt < k)
+            b = m+1;
+          else
+            e=m-1;
       }
-      return ans;
+      return b;
     }
 };
