da68e3bcc07d090bc5e93a96eec737dcddf85270
jatin.py <pandeyjatin47@gmail.com>
2022-08-04 13:59:48 +0530

    Time: 56 ms (46.31%), Space: 13 MB (99.39%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,39 +1,20 @@
-//class Solution {
-//public:
-//    int kthSmallest(vector<vector<int>>&M , int k) {
-//      int b = M[0][0], e = M.back().back();
-//      int ans = -1;
-//      while(b<e){
-//        long long m = (b+e)>>1;
-//        int smlCnt = 0;
-//        for(int i=0; i<M.size(); i++)
-//          smlCnt += upper_bound(M[i].begin(), M[i].end(), m)-M[i].begin();
-//          if (smlCnt >= k)
-//            ans=m, e=m-1;
-//          else
-//            b=m+1;
-//      }
-//      return ans;
-//    }
-//};
-
 class Solution {
 public:
-    int m, n;
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        m = matrix.size(), n = matrix[0].size();
-        int left = matrix[0][0], right = matrix[m-1][n-1], ans = -1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-          int cnt = 0;
-          for(int i=0; i<matrix.size(); i++){
-            cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
-          }
-            if (cnt >= k) {
-                ans = mid;
-                right = mid - 1;
-            } else left = mid + 1;
+    int kthSmallest(vector<vector<int>>&M , int k) {
+      int b = M[0][0], e = M.back().back();
+      int ans = -1;
+      while(b<=e){
+        long long m = (b+e)>>1;
+        int smlCnt = 0;
+        for(int i=0; i<M.size(); i++)
+          smlCnt += upper_bound(M[i].begin(), M[i].end(), m)-M[i].begin();
+        if(smlCnt >= k){
+          ans = m;
+          e = m-1;
         }
-        return ans;
+        else
+          b = m+1;
+      }
+      return ans;
     }
 };
