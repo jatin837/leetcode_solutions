59259749f84794630fadd78a0b7bd538ff1e3772
jatin.py <pandeyjatin47@gmail.com>
2022-08-05 10:28:38 +0530

    Time: 7 ms (19.94%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -2,11 +2,11 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-      unordered_map<ull, ull>dp;
+      vector<unsigned long long>dp(target+1);
       dp[0]=1;
       for(int i=1; i<=target; i++)
         for(auto num:nums)
-          dp[i] += dp[i-num];
+          dp[i] += (i-num>=0)?dp[i-num]:0;
       return dp[target];
     }
 };
