b86d2caa06e7249673f078721430e432bf0b3b6a
jatin.py <pandeyjatin47@gmail.com>
2022-08-05 10:25:58 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,7 +1,8 @@
+#define ull unsigned long long
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-      vector<unsigned long long>dp(target+1);
+      vector<ull>dp(target+1);
       dp[0]=1;
       for(int i=1; i<=target; i++)
         for(auto num:nums)
