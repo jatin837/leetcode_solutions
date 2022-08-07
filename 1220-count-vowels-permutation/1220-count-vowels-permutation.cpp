1c8be65e2f6af511fa8a5837d3fa8dfac8116b60
jatin.py <pandeyjatin47@gmail.com>
2022-08-07 12:58:50 +0530

    Time: 8 ms (83.02%), Space: 6.8 MB (71.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-      long long dp[20000][5]; 
-      for(int i=0; i<5; i++)
-        dp[0][i] = 1;
+      long long dp[20000][5] = {{1, 1, 1, 1, 1}}; 
       long long mod = 1e9+7;
+      cout<<dp[0][3];
+      int ans;
       for(int i=1; i<n; i++){
         dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4])%mod;
         dp[i][1] = (dp[i-1][0] + dp[i-1][2])%mod;
@@ -12,7 +12,6 @@ public:
         dp[i][3] = (dp[i-1][2])%mod;
         dp[i][4] = (dp[i-1][3] + dp[i-1][2])%mod;
       }
-      long long ans = 0;
       for(auto num:dp[n-1])
         ans = (ans + num)%mod;
       return ans;
