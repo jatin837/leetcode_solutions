49ba6ebfd0fdf623cecabc5da258318a274586fe
jatin.py <pandeyjatin47@gmail.com>
2022-08-16 12:27:29 +0530

    Time: 184 ms (76.85%), Space: 9.6 MB (87.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,11 +6,10 @@ public:
     bool dp(int target, int pos, vector<int>&A){
       vector<bool>memo(target+1, false);
       memo[0]=true;
-      for(int n:A){
+      for(int n:A)
         for(int sum = target; sum > 0; sum--)
-          if(sum-n>=0)
+          if(sum>=n)
             memo[sum] = memo[sum] || memo[sum-n];
-      }
       return memo[target];
     }
     bool canPartition(vector<int>& nums) {
