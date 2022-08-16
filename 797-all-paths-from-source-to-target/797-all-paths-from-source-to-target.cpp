b9b244b35ce07d9511a98863fa4bbab9ad2f1c13
jatin.py <pandeyjatin47@gmail.com>
2022-08-16 17:36:01 +0530

    Time: 460 ms (5.02%), Space: 22.7 MB (8.55%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>dfs(vector<vector<int>>& g, int node){
-      if(node == g.size()-1)
-        return {{node}};
-      vector<vector<int>>ans;
-      for(int n:g[node])
-        for(auto gn:dfs(g, n)){
-          gn.insert(gn.begin(), node);
-          ans.push_back(gn);
-        }
-      return ans;
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-      return dfs(graph, 0);
+      queue<vector<int>>q;
+      q.push({0});
+      vector<vector<int>>ret;
+      while(!q.empty()){
+        queue<vector<int>>q2;
+        while(!q.empty()){
+          auto top = q.front();
+          for(int n:top)
+            cout<<n<<' ';
+          cout<<'\n';
+          q.pop();
+          int currNode = top.back();
+          if(currNode == graph.size()-1)
+            ret.push_back(top);
+          for(int nxtNode:graph[currNode]){
+            vector<int>newList = vector<int>(top.begin(), top.end());
+            newList.push_back(nxtNode);
+            q2.push(newList);
+          }
+        }
+        q=q2; 
+      }
+      return ret;
     }
 };
