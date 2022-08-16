8b09a0f7ecbff91e27b49ab6964949bcf8692ff4
jatin.py <pandeyjatin47@gmail.com>
2022-08-16 17:41:22 +0530

    Time: 87 ms (63.37%), Space: 21.8 MB (56.17%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& g){
-      if(i<0 || j<0 || i==g.size() || j==g[0].size() || g[i][j]==0)
-        return 0;
-      g[i][j] = 0;
-      int ans = 1;
-      ans += dfs(i-1, j, g);
-      ans += dfs(i+1, j, g);
-      ans += dfs(i, j-1, g);
-      ans += dfs(i, j+1, g);
-      return ans;
-    }
-    int numEnclaves(vector<vector<int>>& g) {
-      for(int j=0; j<g[0].size(); j++){
-        dfs(0, j, g);
-        dfs(g.size()-1, j, g);
-      }
-      for(int i=0; i<g.size(); i++){
-        dfs(i, 0, g);
-        dfs(i, g[0].size()-1, g);
-      }
-      int cnt = 0;
-      for(int i=0; i<g.size(); i++)
-        for(int j=0; j<g[0].size(); j++)
-          cnt+=dfs(i, j, g);
-      return cnt;
-    }
+int dfs(int i, int j, vector<vector<int>>& g){
+  if(i<0 || j<0 || i==g.size() || j==g[0].size() || g[i][j]==0)
+    return 0;
+  g[i][j] = 0;
+  int ans = 1;
+  ans += dfs(i-1, j, g);
+  ans += dfs(i+1, j, g);
+  ans += dfs(i, j-1, g);
+  ans += dfs(i, j+1, g);
+  return ans;
+}
+int numEnclaves(vector<vector<int>>& g) {
+  for(int j=0; j<g[0].size(); j++){
+    dfs(0, j, g);
+    dfs(g.size()-1, j, g);
+  }
+  for(int i=0; i<g.size(); i++){
+    dfs(i, 0, g);
+    dfs(i, g[0].size()-1, g);
+  }
+  int cnt = 0;
+  for(int i=0; i<g.size(); i++)
+    for(int j=0; j<g[0].size(); j++)
+      cnt+=dfs(i, j, g);
+  return cnt;
+}
 };
