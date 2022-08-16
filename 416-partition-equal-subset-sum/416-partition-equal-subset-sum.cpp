1979ce5ce7927295faef4697e169dd3bc1ddc55a
jatin.py <pandeyjatin47@gmail.com>
2022-08-16 12:21:14 +0530

    Time: 136 ms (85.92%), Space: 9.7 MB (87.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,19 +4,14 @@
 class Solution {
 public:
     bool dp(int target, int pos, vector<int>&A){
-      bool memo[MAX_SUM][MAX_NUM];
-      bool ans = false;
-      for(int i=0; i<=A.size(); i++)
-        memo[0][i]=true;
-      for(int sum = 1; sum <= target; sum++){
-        ans = false;
-        for(int i=0; i<A.size(); i++){
-          bool include = (sum-A[i]<0)?false:memo[sum-A[i]][i], exclude=memo[sum][i];
-          memo[sum][i+1] = include || exclude;
-          ans = ans or memo[sum][i+1];
-        }
+      vector<bool>memo(target+1, false);
+      memo[0]=true;
+      for(int n:A){
+        for(int sum = target; sum > 0; sum--)
+          if(sum-n>=0)
+            memo[sum] = memo[sum] || memo[sum-n];
       }
-      return ans;
+      return memo[target];
     }
     bool canPartition(vector<int>& nums) {
       int target = accumulate(nums.begin(), nums.end(), 0);
