47214d7c980230a7f8e460c505e4f8288d5aeb99
jatin.py <pandeyjatin47@gmail.com>
2022-08-19 11:09:49 +0530

    Time: 195 ms (34.71%), Space: 58.5 MB (41.56%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+      unordered_map<int, int>left, need;
+      for(int n:nums)
+        left[n]++;
+      for(int i=0; i<nums.size(); i++){
+        int currNum = nums[i];
+        cout<<i<<'\n';
+        if(left[currNum]==0)
+          continue;
+        if(need[currNum]){
+          need[currNum]--;
+          left[currNum]--;
+          need[currNum+1]++;
+          continue;
+        }
+        if(left[currNum+1] && left[currNum+2]){
+          left[currNum]--;
+          left[currNum+1]--;
+          left[currNum+2]--;
+          need[currNum+3]++;
+          continue;
+        }
+        return false;
+      }
+      return true;
+    }
+};
