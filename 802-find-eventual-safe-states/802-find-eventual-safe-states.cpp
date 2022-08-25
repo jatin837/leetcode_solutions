d3b197a98cce1d99064f7598cb40d8481ad28f80
jatin.py <pandeyjatin47@gmail.com>
2022-08-25 15:59:27 +0530

    Time: 334 ms (27.01%), Space: 47.1 MB (75.80%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&G, int pos, vector<int>&status, unordered_set<int>&visited){
-      if(status[pos] != 0)
-        return;
-      if(visited.count(pos)){
-        status[pos] = -1;
+    void dfs(vector<vector<int>>&G, int pos, vector<int>&status){
+      if(status[pos] == 2){
+        status[pos] = -1; 
         return;
       }
-      visited.insert(pos);
-      if(G[pos].size()==0){
-        status[pos] = 1;
+      if(status[pos] == 1 || status[pos]==-1)
         return;
-      }
-      for(int n:G[pos])
-        if(status[n] == 0)
-          dfs(G, n, status, visited);
-      for(int n:G[pos])
-        if(status[n] == -1){
-          status[pos] = -1;
+      if(status[pos] == 0){
+        status[pos] = 2;
+        if(G[pos].size()==0){
+          status[pos] = 1;
           return;
         }
-      status[pos] = 1;
+        for(int n:G[pos])
+          if(status[n] == 0)
+            dfs(G, n, status);
+        for(int n:G[pos])
+          if(status[n] == -1 || status[n] == 2){
+            status[pos] = -1;
+            return;
+          }
+        status[pos] = 1;       
+      }
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
       vector<int>ans;
       vector<int>status(graph.size(), 0);
-      unordered_set<int>visited;
       for(int i=0; i<graph.size(); i++){
-        dfs(graph, i, status, visited);
+        dfs(graph, i, status);
         if(status[i] == 1)
           ans.push_back(i);
       }
