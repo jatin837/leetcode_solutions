4059d5bb771f6515b0325372a5d1fff974797713
jatin.py <pandeyjatin47@gmail.com>
2022-08-25 10:39:46 +0530

    Time: 28 ms (47.16%), Space: 9 MB (6.70%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-      unordered_map<char, int>freq1;
-      unordered_map<char, int>freq2;
+      unordered_map<char, int>given;
+      unordered_map<char, int>required;
       for(auto ch:ransomNote)
-        freq1[ch]++;
+        required[ch]++;
       for(auto ch:magazine)
-        freq2[ch]++;
-      for(auto it=freq1.begin(); it!=freq1.end(); it++){
-        char ch = it->first;
-        int cnt = it->second;
-        if(freq2[ch] < cnt)
+        given[ch]++;
+      for(auto [ch, cnt]:required)
+        if(given[ch] < cnt)
           return false;
-      }
       return true;
     }
 };
