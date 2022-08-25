c18800c0f41fa21a4f9daa0f2862d3ac7281cb13
jatin.py <pandeyjatin47@gmail.com>
2022-08-25 12:19:42 +0530

    Time: 200 ms (45.16%), Space: 47.1 MB (33.67%) - LeetHub

---
+++ 851-loud-and-rich/851-loud-and-rich.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>&G, int i, vector<int>&q, vector<int>&memo){
+      if(memo[i] != q.size())
+        return memo[i];
+      int ans = i;
+      for(int child:G[i]){
+        int pos = dfs(G, child, q, memo);
+        if(q[pos] < q[ans])
+          ans = pos;
+      }
+      return ans;
+    }
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+      vector<vector<int>>Graph(quiet.size());
+      vector<int>ret(quiet.size(), quiet.size());
+      for(auto v:richer)
+        Graph[v[1]].push_back(v[0]);
+      for(int i=0; i<quiet.size(); i++){
+        if(ret[i] == quiet.size())
+          ret[i] = dfs(Graph, i, quiet, ret);
+      }
+      return ret;
+    }
+};
