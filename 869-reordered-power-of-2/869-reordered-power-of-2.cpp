39f8e6bbef5e86b8736691ca523b57b59b38e1a3
jatin.py <pandeyjatin47@gmail.com>
2022-08-26 13:27:16 +0530

    Time: 6 ms (50.57%), Space: 6.8 MB (17.51%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-  bool reorderedPowerOf2(int N) {
-    vector<vector<int>>solSpace(30, vector<int>(10, 0));
-    unsigned long n = 1;
-    for(int i=0; i<30; i++){
-      unsigned long num = 1<<i;
-      while(num){
-        solSpace[i][num%10]++; 
-        num/=10;
-      }
-    }
-    vector<int>given(10);
+  vector<int>count(int N){
+    vector<int>ret(10, 0);
     while(N){
-      given[N%10]++;
+      ret[N%10]++;
       N/=10;
     }
+    return ret;
+  }
+  bool reorderedPowerOf2(int N) {
+    vector<vector<int>>solSpace(30, vector<int>(10, 0));
+    vector<int>given(10);
+    for(int i=0; i<30; i++)
+      solSpace[i] = count(1<<i);
+    given = count(N);
     for(int i=0; i<30; i++){
       bool found = true;
       for(int j=0; j<10; j++)
