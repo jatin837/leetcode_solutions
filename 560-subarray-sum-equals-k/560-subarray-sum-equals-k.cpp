0626d3b939e18c24fbc9d43836a96ca516ffa611
jatin.py <pandeyjatin47@gmail.com>
2022-08-27 14:30:25 +0530

    Time: 132 ms (63.77%), Space: 47.1 MB (18.40%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-      unordered_map<int, int>preCount;
-      preCount[0] = 1;
+      unordered_map<int, int>sumCnt;
+      sumCnt[0] = 1;
       int sum = 0;
       int cnt = 0;
       for(int n:nums){
         sum += n;
-        cnt += preCount[sum-k];
-        preCount[sum]++;
+        cnt += sumCnt[sum-k];
+        sumCnt[sum]++;
       }
       return cnt;
     }
