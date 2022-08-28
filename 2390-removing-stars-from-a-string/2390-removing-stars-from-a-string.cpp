ef315b1680dffb7d17c22272ff6bd24e2174004c
jatin.py <pandeyjatin47@gmail.com>
2022-08-28 09:50:21 +0530

    Time: 105 ms (33.33%), Space: 24.1 MB (16.67%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,35 +1,12 @@
-//class Solution {
-//public:
-//    string removeStars(string s) {
-//      int i=0;
-//      while(i<s.length()){
-//        if(s[i] == '*'){
-//          s.erase(i-1,2);
-//          i--;
-//          continue;
-//        }
-//        i++;
-//      }
-//      return s;
-//    }
-//};
-
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> st;
-        for(auto c: s){
-            if(c == '*'){
-                st.pop();
-                continue;
-            }else st.push(c);
-        }
-        string ans(st.size(), 'x');
-        int i=st.size();
-        while(!st.empty()){
-            ans[--i] = st.top();
-            st.pop();
-        }
-        return ans;
+      int j=0;
+      for(int i=0; i<s.length(); i++)
+        if(s[i] == '*')
+          j--;
+        else
+          s[j++] = s[i];
+      return s.substr(0, j);
     }
 };
