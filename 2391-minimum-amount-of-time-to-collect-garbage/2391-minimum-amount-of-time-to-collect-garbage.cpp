c9a30723f9aaf9ee30b9d030f91d4c23e1d0c6a8
jatin.py <pandeyjatin47@gmail.com>
2022-08-28 10:19:52 +0530

    Time: 196 ms (100.00%), Space: 103.7 MB (30.00%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -3,18 +3,19 @@ public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
       int t=0;
       int ret = 0;
-      unordered_map<char, int>last;
+      int last[3]={0};
+      string type = "PGM";
       vector<int>pre(travel.size()+1, 0);
       for(int i=1; i<=travel.size(); i++)
         pre[i] = travel[i-1] + pre[i-1];
       for(int i=0; i<garbage.size(); i++){
         t += garbage[i].length();
         for(char ch:garbage[i])
-          last[ch]=i;
+          for(int j=0; j<3; j++)
+            if(type[j] == ch)
+              last[j]=i;
       }
-      t += pre[last['P']];
-      t += pre[last['M']];
-      t += pre[last['G']];
+      t += pre[last[0]]+pre[last[1]]+pre[last[2]];
       return t;
     }
 };
