7dac5ef607d147fadd6a1fdf7d8ce1aa8d2331bb
jatin.py <pandeyjatin47@gmail.com>
2022-08-29 12:27:20 +0530

    Time: 26 ms (80.16%), Space: 14 MB (51.07%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+    vector<vector<int>>G(numCourses);
+    for(auto e:prereq){
+      if(e[0] == e[1])
+        return false;
+      G[e[1]].push_back(e[0]);
+    }
+    vector<int>degree(numCourses, 0);
+    for(int i=0; i<numCourses; i++)
+      for(int n:G[i])
+        degree[n]++;
+    queue<int>q;
+    for(int i=0; i<numCourses; i++)
+      if(degree[i] == 0)
+        q.push(i);
+//  for(int n:degree)
+//    cout<<n<<' ';
+//  cout<<'\n';
+    if(q.size()==0)
+      return false;
+    while(!q.empty()){
+      int x = q.front();
+      q.pop();
+      for(int n:G[x]){
+        degree[n]--;
+        if(degree[n] == 0)
+          q.push(n);
+        if(degree[n] == -1)
+          return false;
+      }
+    }
+    for(int n:degree)
+      if(n != 0)
+        return false;
+    return true;
+  }
+};
