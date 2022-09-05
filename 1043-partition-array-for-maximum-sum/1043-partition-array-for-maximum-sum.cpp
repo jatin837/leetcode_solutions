40776329a7914cf1ffbd66666dbe9a645498ce10
jatin.py <pandeyjatin47@gmail.com>
2022-09-05 14:13:29 +0530

    Time: 11 ms (97.60%), Space: 8.4 MB (92.65%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-      vector<int>dp(arr.size()+1, 0);
+      int dp[501] = {0};
       for(int i=1; i<arr.size()+1; i++){
         int mx = 0;
         for(int j=1; j<=k && i-j>=0; j++){
@@ -9,6 +9,6 @@ public:
           dp[i] = max(dp[i], dp[i-j] + mx*j);
         }
       }
-      return dp.back();
+      return dp[arr.size()];
     }
 };
