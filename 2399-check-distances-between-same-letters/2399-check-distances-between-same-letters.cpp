e5566ed427cacd7c09275f5677b9bc12bb64db45
jatin.py <pandeyjatin47@gmail.com>
2022-09-05 15:58:34 +0530

    Time: 11 ms (50.00%), Space: 13.1 MB (50.00%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool checkDistances(string s, vector<int>& d) {
       for(int i=0; i<s.length(); i++){
-        char ch = s[i];
-        int nidx = i+d[ch-'a']+1;
-        int pidx = i-d[ch-'a']-1;
-        if((nidx < s.length() && s[nidx] == ch) || (pidx >= 0 && s[pidx] == ch))
+        int nidx = i+d[s[i]-'a']+1;
+        int pidx = i-d[s[i]-'a']-1;
+        if((nidx < s.length() && s[nidx] == s[i]) || (pidx >= 0 && s[pidx] == s[i]))
           continue;
         return false;
       }
