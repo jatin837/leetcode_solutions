df0c8a9cbc9eb372300454168679f2862d06353b
jatin.py <pandeyjatin47@gmail.com>
2022-09-06 18:00:48 +0530

    Time: 11 ms (8.40%), Space: 5.9 MB (75.61%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -10,28 +10,28 @@ public:
       if (dividend == INT_MIN and !neg)
         divd -= 1;
       
-      long s = 1;
+      long b = 1;
       long e = divd;
-      long m = (s+e)>>1;
+      long m = (b+e)>>1;
       
       if (divr == 1)
         if(neg)
           return -1*divd;
         else
           return divd;
-      while(s<e){
+      while(b<e){
         if(divd - m*divr >= 0 and divd - (m+1)*divr < 0){
           if(neg)
             return -1*m;
           return m;
         }else if(divd - m*divr > 0 and divd - (m+1)*divr > 0)
-          s = m+1;
+          b = m+1;
         else
           e = m-1;
-        m = (s+e)>>1;
+        m = (b+e)>>1;
       }
       if(neg)
-        return -1*s;
-      return s;
+        return -1*b;
+      return b;
     }
 };
