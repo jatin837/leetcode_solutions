775dc7a0ac997a6814335e7043de3b22de01b8be
jatin.py <pandeyjatin47@gmail.com>
2022-09-06 19:34:01 +0530

    Time: 86 ms (88.94%), Space: 21.9 MB (65.01%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    bool valid(string &s, array<bool, 26>&fq){
-      for(char ch:s)
-        if(fq[ch-'a'])
-          return false;
-      return true;
-    }
     int maxProduct(vector<string>& words) {
-      vector<array<bool, 26>>mp(words.size());
+      vector<int>cache(words.size());
       int ans = 0;
       for(int i=0; i<words.size(); i++){
         for(char ch:words[i])
-          mp[i][ch-'a']=true;
+          cache[i] |= 1<<(ch-'a');
         for(int j=0; j<i; j++)
-          if(valid(words[j], mp[i]))
+          if((cache[i]&cache[j])==0)
             ans = max(ans, int(words[i].length()*words[j].length()));
       }
       return ans;
