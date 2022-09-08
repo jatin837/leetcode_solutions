6cdf2ccee813775f3829ef7f39c5824db1e97c20
jatin.py <pandeyjatin47@gmail.com>
2022-09-08 16:23:04 +0530

    Time: 295 ms (24.31%), Space: 7 MB (95.53%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    long long dp[1001] = {-1};
     long long power(int n){
       if(n == 1)
         return 0;
@@ -9,10 +8,9 @@ public:
       return 1+power(n/2);
     }
     int getKth(int lo, int hi, int k) {
-      vector<int>v;
-      for(int i=lo; i<=hi; i++)
-        v.push_back(i);
-      sort(v.begin(), v.end(), [&](int &a, int &b){
+      vector<int>v(hi-lo+1);
+      iota(v.begin(), v.end(), lo);
+      nth_element(v.begin(), v.begin()+k-1, v.end(), [&](int &a, int &b){
         if(power(a)==power(b))
           return a<=b;
         return power(a) < power(b);
