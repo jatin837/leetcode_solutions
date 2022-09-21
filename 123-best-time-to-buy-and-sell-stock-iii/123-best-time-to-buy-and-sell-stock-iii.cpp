ef7d7148251eb3c5bc8e99f64cbaaab54fc0c8ca
jatin.py <pandeyjatin47@gmail.com>
2022-09-21 20:43:00 +0530

    Time: 485 ms (49.85%), Space: 81.5 MB (63.38%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  int maxProfit(vector<int>& prices) {
+    vector<int>left(prices.size(), 0);
+    vector<int>right(prices.size(), 0);
+    int minSoFar = prices[0];
+    int maxSoFar = prices.back();
+    for(int i=1; i<left.size(); i++){
+      minSoFar = min(prices[i], minSoFar);
+      left[i] = max(left[i-1], prices[i]-minSoFar);
+    }
+    for(int i=prices.size()-2; i>=0; i--){
+      maxSoFar = max(prices[i], maxSoFar);
+      right[i] = max(right[i+1], maxSoFar-prices[i]);
+    }
+    int ret = 0;
+/// for(int n:left)
+///   cout<<n<<' ';
+/// cout<<'\n';
+/// for(int n:right)
+///   cout<<n<<' ';
+/// cout<<'\n';
+    for(int i=0; i<prices.size(); i++){
+      ret = max(ret, right[i]+left[i]);
+    }
+    return ret;
+  }
+};
