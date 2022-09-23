83bb53cb788c9a6ff728756e6b0c825f5fe789f9
jatin.py <pandeyjatin47@gmail.com>
2022-09-23 11:03:41 +0530

    Time: 1492 ms (85.04%), Space: 13.9 MB (62.20%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -4,7 +4,7 @@ class Solution:
       shift = 0
       MOD = int(1e9+7)
       for i in range(1, n+1):
-        if(bin(i).count('1') == 1):
+        if(i&(i-1) == 0):
           shift+=1
         ret = ((ret<<shift)|i)%MOD
       return ret
