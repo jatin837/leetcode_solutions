2a66da0aa1f0241ef10a441a8a5c9b73d2dc922c
jatin.py <pandeyjatin47@gmail.com>
2022-09-25 12:19:04 +0530

    Time: 59 ms (16.62%), Space: 16.9 MB (11.06%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -4,41 +4,43 @@ public:
     int k=0;
     int front = -1;
     int rear = -1;
-    int size = 0;
     MyCircularQueue(int k) {
       this->k = k;
     }
     bool enQueue(int value) {
-      if(this->size == this->k)
+      if(this->isFull())
         return false;
-      if(this->front == -1)
+      if(this->isEmpty())
         this->front = 0;
       this->rear = (this->rear+1)%k;
-      this->size++;
       this->arr[this->rear]=value;
       return true;
     }
     bool deQueue() {
-      if(this->size == 0)
+      if(this->isEmpty())
         return false;
+      if(this->front == this->rear){
+        this->front = -1;
+        this->rear = -1;
+        return true;
+      }
       this->front = (front+1)%this->k;
-      this->size--;
       return true;
     }
     int Front() {
-      if(this->size == 0)
+      if(this->isEmpty())
         return -1;
       return this->arr[this->front];
     }
     int Rear() {
-      if(this->size == 0)
+      if(this->isEmpty())
         return -1;
       return this->arr[this->rear];
     }
     bool isEmpty() {
-      return this->size == 0;
+      return this->front == -1 && this->rear == -1;
     }
     bool isFull() {
-      return this->size == this->k;
+      return (this->rear+1)%this->k == this->front;
     }
 };
