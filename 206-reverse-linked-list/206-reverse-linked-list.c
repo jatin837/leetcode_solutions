94c148d0af56a64905c069184d7a67848bef45ca
jatin.py <pandeyjatin47@gmail.com>
2022-09-26 16:56:48 +0530

    Time: 7 ms (34.69%), Space: 6.8 MB (6.38%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.c
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -8,12 +8,13 @@
 
 
 struct ListNode* reverseList(struct ListNode* head){
-  struct ListNode *curr = head, *prev = NULL; 
-  while(curr){
-    struct ListNode *tmp = curr->next;
-    curr->next = prev;
-    prev = curr;
-    curr = tmp;
-  }
-  return prev;
+  if(head == NULL)
+    return NULL;
+  if(head->next == NULL)
+    return head;
+  struct ListNode *front = reverseList(head->next);
+  struct ListNode *back = head->next;
+  back->next = head;
+  head->next = NULL;
+  return front;
 }
