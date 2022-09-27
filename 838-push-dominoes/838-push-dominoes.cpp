865d18013b60cca05b214eff00bc3200d16a7858
jatin.py <pandeyjatin47@gmail.com>
2022-09-27 16:18:15 +0530

    Time: 110 ms (20.62%), Space: 18.1 MB (51.30%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -5,7 +5,7 @@ public:
     for(int i=0;i<dominoes.size();i++)
       if(dominoes[i]=='L' || dominoes[i]=='R')
         q.push(i);
-    string ans = dominoes;
+    string tmp = dominoes;
     while(!q.empty()){
       int s = q.size();
       for(int i=0;i<s;i++){
@@ -13,24 +13,24 @@ public:
         q.pop();
         if(p-1>=0 && dominoes[p]=='L' && dominoes[p-1]=='.'){
           if(p-2>=0 && dominoes[p-2]!='R'){
-            ans[p-1] = 'L';
+            tmp[p-1] = 'L';
             q.push(p-1);    
           }
           if(p-2<0)
-            ans[p-1] = 'L';
+            tmp[p-1] = 'L';
         }
         if(p+1<dominoes.size() && dominoes[p]=='R' && dominoes[p+1]=='.'){
           if(p+2<dominoes.size() && (dominoes[p+2]!='L')) {
-            ans[p+1] = 'R';
+            tmp[p+1] = 'R';
             q.push(p+1);
           }         
           if(p+2>=dominoes.size()){
-            ans[p+1] = 'R';
+            tmp[p+1] = 'R';
           }
         }
       }
-      dominoes = ans;
+      dominoes = tmp;
     }
-  return dominoes;
-}
+    return dominoes;
+  }
 };
