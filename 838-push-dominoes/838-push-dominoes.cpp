0e24c4bee4efcc7f1057383a862fd9e2b67d5617
jatin.py <pandeyjatin47@gmail.com>
2022-09-27 16:08:42 +0530

    Time: 74 ms (50.95%), Space: 18.1 MB (51.30%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -12,22 +12,20 @@ public:
         auto p = q.front();
         q.pop();
         if(p-1>=0 && dominoes[p]=='L' && dominoes[p-1]=='.'){
-          if(p-2>=0 && (dominoes[p-2]=='.' || dominoes[p-2]=='L')){
+          if(p-2>=0 && dominoes[p-2]!='R'){
             ans[p-1] = 'L';
             q.push(p-1);    
           }
-          if(p-2<0){
+          if(p-2<0)
             ans[p-1] = 'L';
-          }
         }
         if(p+1<dominoes.size() && dominoes[p]=='R' && dominoes[p+1]=='.'){
-          if(p+2<dominoes.size() && (dominoes[p+2]=='.' || dominoes[p+2]=='R')) {
+          if(p+2<dominoes.size() && (dominoes[p+2]!='L')) {
             ans[p+1] = 'R';
             q.push(p+1);
           }         
-          if(p+2>=dominoes.size()){
+          if(p+2>=dominoes.size())
             ans[p+1] = 'R';
-          }
         }
       }
       dominoes = ans;
