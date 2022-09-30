f0abcb6faf2c988d964fcee8e66229283a2cae9d
jatin.py <pandeyjatin47@gmail.com>
2022-09-30 10:37:16 +0530

    Time: 116 ms (12.37%), Space: 18.2 MB (17.04%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+    vector<vector<int>>pts;
+    vector<vector<int>>ret;
+    multiset<int>active;
+    active.insert(0);
+    for(int i=0; i<buildings.size(); i++){
+      pts.push_back({buildings[i][0], -buildings[i][2]});
+      pts.push_back({buildings[i][1], buildings[i][2]});
+    }
+    sort(pts.begin(), pts.end());
+    int prev = 0;
+    int curr = 0;
+    for(auto pt:pts){
+      if(pt[1]>0)
+        active.erase(active.find(pt[1]));
+      else
+        active.insert(-pt[1]);
+      curr = *active.rbegin();
+      if(curr != prev){
+        ret.push_back({pt[0], curr});
+        prev = curr;
+      }
+    }
+    return ret;
+  }
+};
