2d56ce50c89ff379f731cb3f87bf4cbb43d27757
jatin.py <pandeyjatin47@gmail.com>
2022-10-01 10:03:38 +0530

    Time: 4 ms (47.86%), Space: 6.2 MB (75.45%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,63 +1,23 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-      
-      if (s[0] == '0')
-        return 0;
-      
-      int n = s.length();
-      vector<int> dp(n, 0);
-      
-      dp[0] = 1;
-      
-      if (n == 1)
-        return dp[0];
-      
-      
-      if (s[0] == '1'){
-        if (s[1] == '0')
-          dp[1] = 1;
-        else
-          dp[1] = 2;
+  int numDecodings(string s) {
+    vector<int>memo(s.length()+1, -1);
+    return dp(s, 0, memo);
+  }
+  int dp(string &s, int pos, vector<int>&memo){
+    if(pos == s.length())
+      return 1;
+    if(s[pos] == '0')
+      return 0;
+    if(memo[pos] == -1){
+      int ret = dp(s, pos+1, memo);
+      if(pos+1<s.length()){
+        if((s[pos] == '1') || (s[pos] == '2' && s[pos+1] <= '6'))
+          ret += dp(s, pos+2, memo);
       }
-       
-      if (s[0] == '2'){
-        if (s[1] >= '1' and s[1] <= '6')
-          dp[1] = 2;
-        else if (s[1] == '0')
-          dp[1] = 1;
-        else
-          dp[1] = 1;
-      }
-      
-      if (s[0] >= '3'){
-        if (s[1] == '0')
-          return 0;
-        else
-          dp[1] = 1;
-      }
-      for(int i=2; i<n; i++){
-        if (s[i] == '0'){
-          if (s[i-1] == '1' or s[i-1] == '2')
-            dp[i] = dp[i-2];
-          else
-            return 0;
-        }
-        if (s[i] >= '1' and s[i] <= '6'){
-          if (s[i-1] == '2' or s[i-1] == '1')
-            dp[i] = dp[i-2] + dp[i-1];
-          else
-            dp[i] = dp[i-1];
-        }
-        if (s[i] >= '7'){
-          if (s[i-1] == '1')
-            dp[i] = dp[i-2] + dp[i-1];
-          else if (s[i-1] == '0')
-            dp[i] = dp[i-2];
-          else
-            dp[i] = dp[i-1];
-        }
-      }
-      return dp[n-1];
+      memo[pos] = ret;
     }
+
+    return memo[pos];
+  }
 };
