ea3f651e9e1b0831168fb231c0b2689dd16696b8
jatin.py <pandeyjatin47@gmail.com>
2022-10-07 11:21:31 +0530

    Time: 118 ms (92.74%), Space: 26.4 MB (37.06%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,22 @@
+class MyCalendarThree {
+public:
+    map<int, int>diff;
+    MyCalendarThree() {}
+    int book(int start, int end) {
+      this->diff[start]++;
+      this->diff[end]--;
+      int ret = 0;
+      int curr = 0;
+      for(auto [x, y]:diff){
+        curr += y;
+        ret = max(ret, curr);
+      }
+      return ret;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
