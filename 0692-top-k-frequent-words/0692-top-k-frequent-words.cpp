bed05611fc3f47796d0482f5dcef3ab7112e5699
jatin.py <pandeyjatin47@gmail.com>
2022-10-19 19:44:47 +0530

    Time: 30 ms (34.28%), Space: 12.6 MB (56.15%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> hashmap;
+        for(string& word : words) {
+            hashmap[word] += 1;
+        }
+        priority_queue<pair<int, string>, vector<pair<int, string>>, MyComp> pq;
+        for(auto [word, cnt]:hashmap) {
+            pq.push({cnt, word});
+            if(pq.size() > k) 
+              pq.pop();
+        }
+        vector<string> res;
+        while(!pq.empty()) {
+            res.insert(res.begin(), pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+private:
+    struct MyComp {
+        bool operator() (const pair<int, string>& a, const pair<int, string>& b) {
+            if(a.first != b.first) {
+                return a.first > b.first;
+            }
+            else {
+                return a.second < b.second;
+            }
+        }
+    };
+};
