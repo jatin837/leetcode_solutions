97d460f049c1f791273b5bfab155a836dea336a8
jatin.py <pandeyjatin47@gmail.com>
2022-10-21 15:41:15 +0530

    Time: 19 ms (94.34%), Space: 9.8 MB (91.81%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -6,26 +6,21 @@ public:
     return dp(s1, s2, 0, 0, s1.size());
   }
   bool dp(string& s1, string& s2, int i, int j, int l){
-    if(l == 0)
+    if(l == 0 || memo[i][j][l] == 0)
       return false;
-    if(memo[i][j][l] == 1)
-      return true;
-    if(memo[i][j][l] == 0)
-      return false;
-
-    if(s1.substr(i, l) == s2.substr(j, l)){
-      memo[i][j][l] = 1;
-    }else{
-      for(int k=1; k<=l-1; k++){
-        if((dp(s1, s2, i, j, k) && dp(s1, s2, i+k, j+k, l-k)) || (dp(s1, s2, i, j+l-k, k) && dp(s1, s2, i+k, j, l-k))){
-          memo[i][j][l] = 1;break;
-        }
-      }
+    if(memo[i][j][l] == -1){
+      if(s1.substr(i, l) == s2.substr(j, l))
+        memo[i][j][l] = 1;
+    
+      else
+        for(int k=1; k<=l-1; k++)
+          if((dp(s1, s2, i, j, k) && dp(s1, s2, i+k, j+k, l-k)) || (dp(s1, s2, i, j+l-k, k) && dp(s1, s2, i+k, j, l-k))){
+            memo[i][j][l] = 1;break;
+          }
+    
+      if(memo[i][j][l] != 1)
+        memo[i][j][l] = 0;     
     }
-    
-    if(memo[i][j][l] != 1)
-      memo[i][j][l] = 0;
-    
     return memo[i][j][l] == 1;
   }
 };
