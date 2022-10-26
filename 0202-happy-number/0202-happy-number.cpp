9df811767f687571cef1adc57ac3a42c45921662
jatin.py <pandeyjatin47@gmail.com>
2022-10-26 16:41:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (61.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-  unordered_set<int>seen;
-  bool isHappy(int n) {
-    seen.insert(n);
+  int sumSq(int n){
     int sum = 0;
     while(n){
       int d = n%10;
       sum += d*d;
       n/=10;
+    }   
+    return sum;
+  }
+  bool isHappy(int n) {
+    int slow = n;
+    int fast = sumSq(n);
+    while(fast != slow){
+      if(slow == 1 || fast == 1)
+        return true;
+      slow = sumSq(slow);
+      fast = sumSq(sumSq(fast));
     }
-    if(sum == 1)
-      return true;
-    if(seen.count(sum))
-      return false;
-    return isHappy(sum);
+    return slow == 1 || fast == 1;
   }
 };
