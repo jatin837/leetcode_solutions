5fefc28f34d8d37cf8ade3d0fa546f1e18190ae0
jatin.py <pandeyjatin47@gmail.com>
2022-10-29 11:34:07 +0530

    Time: 1527 ms (5.02%), Space: 168.6 MB (29.92%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+      vector<pair<int,int>>grow_plant_time;
+      
+      for(int i=0; i<plantTime.size(); i++)
+        grow_plant_time.push_back({growTime[i], plantTime[i]});
+      
+      sort(grow_plant_time.rbegin(), grow_plant_time.rend());
+      
+      int days = grow_plant_time[0].first+grow_plant_time[0].second+1;
+      int p0 = grow_plant_time[0].second;
+      
+      cout<<days<<'\n';
+      for(int i=1; i<grow_plant_time.size(); i++){
+        auto [g, p] = grow_plant_time[i];
+        days += max(0, (g+p+p0+1)-days);
+        p0 += p;
+        cout<<days<<'\n';
+      }
+      return days-1;
+    }
+};
