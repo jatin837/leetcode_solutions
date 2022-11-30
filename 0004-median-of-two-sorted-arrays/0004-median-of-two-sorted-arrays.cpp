f1289783f867046044a830a3f6c0e668a8d612f0
jatin.py <pandeyjatin47@gmail.com>
2022-11-30 20:18:42 +0530

    Time: 40 ms (93.53%), Space: 89.3 MB (69.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,25 +3,17 @@ public:
   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     int m = nums1.size();
     int n = nums2.size();
-    
     if(m > n)
         return findMedianSortedArrays(nums2, nums1);
-    
-    
     int s = 0;
     int e = m-1;
-    
     while(true){
       int ma = (s+e)>>1; // mid position in nums1 array
       int mb = (m+n)/2 - (ma + 1) - 1; // mid position in nums2 array
-      
-
       int Aleft = (ma>=0)?nums1[ma]:INT_MIN;
       int Aright = (ma+1>=nums1.size())?INT_MAX:nums1[ma+1];
-        
       int Bleft = (mb>=0)?nums2[mb]:INT_MIN;
       int Bright = (mb+1>=nums2.size())?INT_MAX:nums2[mb+1];
-      
       if (Aleft <= Bright and Bleft <= Aright){
         if ((m+n)&1)
           return min(Aright, Bright);
