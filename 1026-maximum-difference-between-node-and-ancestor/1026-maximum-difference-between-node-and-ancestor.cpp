9ada9453ba8bae7af2a17cdfd512b17d2e93cc92
jatin.py <pandeyjatin47@gmail.com>
2022-12-09 18:10:29 +0530

    Time: 6 ms (80.83%), Space: 9.8 MB (80.27%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-  int maxAncestorDiff(TreeNode* root) {
-    stack<pair<TreeNode*, array<int, 2>>> st;
-    st.push({root, {root->val, root->val}});
-    int ret = 0;
-    while(st.size()){
-      auto top = st.top();
-      TreeNode* node = top.first;
-      st.pop();
-      if(node){
-        int mn = min(top.second[0], node->val);
-        int mx = max(top.second[1], node->val);
-        if(node->left || node->right)
-          st.push({node->left, {mn, mx}}), st.push({node->right, {mn, mx}});
-        else
-          ret = max(ret, abs(mn-mx));
-      }
+  int ret = INT_MIN;
+  void dfs(TreeNode* root, int mn, int mx){
+    if(root){
+      mn = min(root->val, mn);
+      mx = max(root->val, mx);
+      ret = max(ret, abs(mx-mn));
+      dfs(root->left, mn, mx);
+      dfs(root->right, mn, mx);
     }
+  }
+  int maxAncestorDiff(TreeNode* root) {
+    dfs(root, root->val, root->val);
     return ret;
   }
 };
