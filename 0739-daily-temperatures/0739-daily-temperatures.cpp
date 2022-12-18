22350975a0031dee3bec1b8b085ec2d8fa0d823e
jatin.py <pandeyjatin47@gmail.com>
2022-12-18 20:20:09 +0530

    Time: 470 ms (28.26%), Space: 102.9 MB (49.40%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,15 +1,23 @@
+/*
+- This problem wants closest element on the right of the array which is strictly greater than the current index.
+- So, ultimilately, this is a Monotonic stack problem
+- Basically, we need to build the monotonically decreasing stack while scanning each element in the array
+*/
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temps) {
-      vector<int>ret(temps.size(), 0);
       stack<int>monoSt;
       for(int i=0; i<temps.size(); i++){
         while(monoSt.size() && temps[monoSt.top()] < temps[i]){
-          ret[monoSt.top()] = i-monoSt.top();
+          temps[monoSt.top()] = i-monoSt.top();
           monoSt.pop();
         }
         monoSt.push(i);
       }
-      return ret;
+      while(monoSt.size()){
+        temps[monoSt.top()] = 0;
+        monoSt.pop();
+      }
+      return temps;
     }
 };
