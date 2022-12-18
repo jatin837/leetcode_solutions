2ccc7e930c4787346bb5edbef7b7e3da9a12beea
jatin.py <pandeyjatin47@gmail.com>
2022-12-18 20:15:10 +0530

    Time: 228 ms (83.67%), Space: 103 MB (49.40%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temps) {
-      vector<int>ret(temps.size(), temps.size());
+      vector<int>ret(temps.size(), 0);
       stack<int>monoSt;
       for(int i=0; i<temps.size(); i++){
         while(monoSt.size() && temps[monoSt.top()] < temps[i]){
-          ret[monoSt.top()] = i;
+          ret[monoSt.top()] = i-monoSt.top();
           monoSt.pop();
         }
         monoSt.push(i);
-      }
-      for(int i=0; i<ret.size(); i++){
-        if(ret[i] != temps.size())
-          ret[i] -= i;
-        else
-          ret[i] = 0;
       }
       return ret;
     }
