1428f829ff7b06561763aa0f4b62f241ccd91066
jatin.py <pandeyjatin47@gmail.com>
2022-12-19 14:44:09 +0530

    Time: 804 ms (80.07%), Space: 172.7 MB (53.97%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&G, int node, int dest, unordered_set<int>& visited){
-      bool ret = false;
-      if(node == dest){
-        ret = true;
-      }
-      else{
-        visited.insert(node);
-        for(int n:G[node]){
+    bool dfs(vector<vector<int>>&G, int beg, int dest){
+      stack<int>st;
+      unordered_set<int>visited;
+      st.push(beg);
+      while(st.size()){
+        int currNode = st.top();
+        if(currNode == dest)
+          return true;
+        st.pop();
+        visited.insert(currNode);
+        for(int n:G[currNode])
           if(!visited.count(n))
-            ret = ret || dfs(G, n, dest, visited);
-        }
+            st.push(n);
       }
-      return ret;
+      return false;
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
       vector<vector<int>>G(n);
       for(auto edge:edges)
         G[edge[0]].push_back(edge[1]),G[edge[1]].push_back(edge[0]);
-      unordered_set<int>visited;
-      return dfs(G, source, destination, visited);
+      return dfs(G, source, destination);
     }
 };
