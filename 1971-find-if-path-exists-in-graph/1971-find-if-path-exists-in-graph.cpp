f12c7a40c4a27cd4d41e9718aa4bca9d7b281a52
jatin.py <pandeyjatin47@gmail.com>
2022-12-19 14:45:14 +0530

    Time: 836 ms (77.65%), Space: 164.9 MB (58.82%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&G, int beg, int dest){
-      stack<int>st;
+    bool bfs(vector<vector<int>>&G, int beg, int dest){
+      queue<int>st;
       unordered_set<int>visited;
       st.push(beg);
       while(st.size()){
-        int currNode = st.top();
+        int currNode = st.front();
         if(currNode == dest)
           return true;
         st.pop();
@@ -20,6 +20,6 @@ public:
       vector<vector<int>>G(n);
       for(auto edge:edges)
         G[edge[0]].push_back(edge[1]),G[edge[1]].push_back(edge[0]);
-      return dfs(G, source, destination);
+      return bfs(G, source, destination);
     }
 };
