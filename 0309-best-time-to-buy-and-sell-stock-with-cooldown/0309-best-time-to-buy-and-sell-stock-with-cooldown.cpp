6c3ff5d5f11f4445c59aa126038ea896c65adf8b
jatin.py <pandeyjatin47@gmail.com>
2022-12-23 15:17:32 +0530

    Time: 7 ms (65.82%), Space: 11.2 MB (91.01%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,14 @@
 class Solution {
 private:
   int dp[5001][2];
-  int f(int day, bool buy, vector<int>&prices){
-    if(day >= prices.size())
-      return 0;
-    if(dp[day][buy] != -1)
-      return dp[day][buy];
-    if(buy) dp[day][buy] = max(-prices[day] + f(day+1, false, prices), f(day+1, true, prices));
-    else dp[day][buy] = max(prices[day]+f(day+2, true, prices), f(day+1, false, prices));
-    return dp[day][buy];
-  }
 public:
     int maxProfit(vector<int>& prices) {
-      memset(dp, -1, sizeof(dp));
-      return f(0, true, prices);
+      int n = prices.size();
+      memset(dp, 0, sizeof(dp));
+      for(int i=n-1; i>=0; i--){
+        dp[i][0] = max(dp[i+2][1] + prices[i], dp[i+1][0]);
+        dp[i][1] = max(dp[i+1][0] - prices[i], dp[i+1][1]);
+      }
+      return dp[0][1];
     }
 };
