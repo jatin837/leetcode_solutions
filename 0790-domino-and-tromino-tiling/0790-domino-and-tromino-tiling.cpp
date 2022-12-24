7431b9b2479da34aa6cc3974024ef85647639df3
jatin.py <pandeyjatin47@gmail.com>
2022-12-24 14:46:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.59%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -3,15 +3,25 @@ public:
     int numTilings(int n) {
       long long dp[1001][2];
       memset(dp, 0, sizeof(dp));
-      dp[1][0] = 1;
-      dp[1][1] = 0;
-      dp[2][0] = 2;
-      dp[2][1] = 1;
+      long long dp10 = 1;
+      long long dp11 = 0;
+      long long dp20 = 2;
+      long long dp21 = 1;
+      long long dp31 = 1;
+      long long dp30 = 1;
+      if(n == 1)
+        return dp10;
+      if(n == 2)
+        return dp20;
       int mod = 1e9+7;
       for(int i=3; i<=n; i++){
-        dp[i][0] = (dp[i-1][0]%mod + dp[i-2][0]%mod + 2*dp[i-1][1]%mod)%mod;
-        dp[i][1] = (dp[i-1][1]%mod + dp[i-2][0]%mod)%mod;
+        dp30 = (dp20 + dp10 + 2*dp21)%mod;
+        dp31 = (dp21 + dp10)%mod;
+        dp10 = (dp20)%mod;
+        dp11 = (dp21)%mod;
+        dp20 = (dp30)%mod;
+        dp21 = (dp31)%mod;
       }
-      return dp[n][0]%mod;
+      return dp30;
     }
 };
