966db4e4eb1b17fb3a7a881a83872a3c253bc1ae
jatin.py <pandeyjatin47@gmail.com>
2022-12-25 10:11:57 +0530

    Time: 15 ms (96.60%), Space: 14.1 MB (12.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-      vector<int>ans;
-      sort(nums.begin(), nums.end());
-      vector<int>preSum(nums.size(), 0);
-      preSum[0] = nums[0];
-      
-      for(int i=1; i<nums.size(); i++)
-        preSum[i] = preSum[i-1]+nums[i];
-      
-      vector<int>ret;
-      for(int q:queries){
-        int idx = upper_bound(preSum.begin(), preSum.end(), q)-preSum.begin();
-        ret.push_back(idx);
-      }
-      return ret;
-    }
+  vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+    vector<int>ans;
+    sort(nums.begin(), nums.end());
+    vector<int>preSum(nums.size(), 0);
+    preSum[0] = nums[0];
+    for(int i=1; i<nums.size(); i++)
+      preSum[i] = preSum[i-1]+nums[i];
+    vector<int>ret;
+    for(int q:queries)
+      ret.push_back(upper_bound(preSum.begin(), preSum.end(), q)-preSum.begin());
+    return ret;
+  }
 };
