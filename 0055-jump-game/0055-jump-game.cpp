cea7c7a2913ef2d19f070c8b9a059ac12c00649a
jatin.py <pandeyjatin47@gmail.com>
2022-12-26 13:57:35 +0530

    Time: 74 ms (85.59%), Space: 48.5 MB (48.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,9 +3,11 @@ public:
   bool canJump(vector<int>& nums) {
     int next = 1;
     for(int i=0; i<nums.size(); i++){
-      if(next == 0)
-        return false;
-      next = max(next-1, nums[i]);
+      if(next){
+        next = max(next-1, nums[i]);
+        continue;
+      }
+      return false;
     }
     return true;
   }
