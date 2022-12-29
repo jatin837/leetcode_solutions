8cb35630378a12eac09aafa62f0ef644d523fd4f
jatin.py <pandeyjatin47@gmail.com>
2022-12-29 12:09:41 +0530

    Time: 687 ms (82.27%), Space: 129.5 MB (51.59%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-  vector<int> getOrder(vector<vector<int>>& A) {
-    priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>> pq;
-    int N = A.size();
-    long long time = 0, i = 0;
-    for (int i = 0; i < N; ++i)
-      A[i].push_back(i);
-    sort(begin(A), end(A));
-    vector<int> ans;
-    while (i < N || pq.size()) {
-      while (i < N && A[i][0] <= time){
-        pq.push({A[i][1], A[i][2]});
-        i++;
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+      priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>>pq;
+      vector<int>ret;
+      for(int i=0; i<tasks.size(); i++)
+        tasks[i].push_back(i);
+      sort(begin(tasks), end(tasks));
+      long long i=0, time=0;
+      while(pq.size() || i < tasks.size()){
+        while(i < tasks.size() && tasks[i][0] <= time){
+          pq.push({tasks[i][1], tasks[i][2]});
+          i++;
+        }
+        if(pq.empty()){
+          time = tasks[i][0];
+          continue;
+        }
+        auto [procTime, indx] = pq.top();
+        pq.pop();
+        time += procTime;
+        ret.push_back(indx);
       }
-      if(pq.empty()){
-        time = A[i][0];
-        continue;
-      }
-      auto [procTime, indx] = pq.top();
-      pq.pop();
-      time += procTime;
-      ans.push_back(indx);
+      return ret;
     }
-    return ans;
-  }
 };
