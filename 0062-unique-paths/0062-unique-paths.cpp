da05dda8b933890c761398aba4e1f58166e4df72
jatin.py <pandeyjatin47@gmail.com>
2022-12-31 09:22:48 +0530

    Time: 3 ms (44.56%), Space: 5.9 MB (94.92%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  int memo[101][101] = {0};
+  int uniquePaths(int m, int n) {
+    if(m <= 0 || n <= 0)
+      return 0;
+    if(m == 1 || n == 1)
+      memo[m][n] = 1;
+    if(memo[m][n])
+      return memo[m][n];
+    memo[m][n] = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+    return memo[m][n];
+  }
+};
