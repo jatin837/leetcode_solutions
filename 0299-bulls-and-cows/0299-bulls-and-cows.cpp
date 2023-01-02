4def03d278b5c5bb0c23750631c0a36338e32e69
jatin.py <pandeyjatin47@gmail.com>
2023-01-02 13:23:00 +0530

    Time: 6 ms (71.07%), Space: 6.6 MB (41.98%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -3,21 +3,18 @@ public:
     string getHint(string A, string B) {
       int bulls = 0;
       int cows = 0;
-      int fq[10] = {0};
-      for(int i=0; i<A.size(); i++) fq[A[i]-'0']++;
+      int fqA[10] = {0};
+      int fqB[10] = {0};
       for(int i=0; i<A.size(); i++){
         if(A[i] == B[i]){
           bulls++;
-          fq[A[i]-'0']--;
-          A[i] = '-';
+        }else{
+          fqA[A[i]-'0']++;
+          fqB[B[i]-'0']++;
         }
       }
-      for(int i=0; i<A.size(); i++){
-        if(A[i] != '-' && fq[B[i]-'0'] > 0){
-          cows++;
-          fq[B[i]-'0']--;
-        }
-      }
+      for(int i=0; i<10; i++)
+        cows += min(fqA[i], fqB[i]);
       string ret = to_string(bulls) + "A" + to_string(cows) + "B";
       return ret;
     }
