742a423220b3841e285d35a04f5172206e6a5ed0
jatin.py <pandeyjatin47@gmail.com>
2023-01-02 16:36:37 +0530

    Time: 22 ms (49.97%), Space: 7.1 MB (54.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int myMax(int fq[]){
-      int ret = 0;
-      for(int i=0; i<26; i++)
-        ret = max(ret, fq[i]);
-      return ret;
-    }
     int characterReplacement(string s, int k) {
-      int fq[26] = {0};
+      vector<int> fq(26,0);
       int ret = 0;
       int left = 0;
       int right = 0;
       fq[s[right]-'A']++;
       while(left < s.length() && right < s.length()){
         int len = right - left + 1;
-        int ops = len - myMax(fq);
+        int ops = len - *max_element(fq.begin(), fq.end());
         if(ops <= k){
           ret = max(len, ret);
           right++;
