7233ad37e2a210d96043c72f23eeea08f7d73c40
jatin.py <pandeyjatin47@gmail.com>
2023-01-04 08:50:18 +0530

    Time: 256 ms (73.66%), Space: 94.8 MB (96.59%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-      unordered_map<int, int>mp;
-      for(auto task:tasks)
-        mp[task]++;
       int round = 0;
-      for(auto [x, y]:mp){
-        if (y<2)
+      sort(tasks.begin(), tasks.end());
+      int p1=0;
+      int p2=0;
+      while(p1<tasks.size() && p2<tasks.size()){
+        while(p2 < tasks.size() && tasks[p1] == tasks[p2])
+          p2++;
+        if(p2-p1 < 2)
           return -1;
-        int q = y/3;
-        int r = y%3;
+        int q = (p2-p1)/3;
+        int r = (p2-p1)%3;
         round += q + int(r != 0);
+        p1=p2;
       }
       return round;
     }
