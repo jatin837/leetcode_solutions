241db340d23f73d1d17579e1ae3331aa336b4e02
jatin.py <pandeyjatin47@gmail.com>
2023-01-08 09:08:16 +0530

    Time: 124 ms (33.00%), Space: 26.7 MB (10.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,17 +1,25 @@
 class Solution {
+  int gcd(int a, int b) {
+    while (b) {
+      a = a % b;
+      swap(a, b);
+    }
+    return a;
+} 
 public:
     int maxPoints(vector<vector<int>>& points) {
       int ret = 1;
       for(int i=0; i<points.size(); i++){
-        unordered_map<double, int> mp;
+        unordered_map<string, int> mp;
         for(int j=0; j<points.size(); j++){
           if(j != i){
             int x1 = points[i][0];
             int y1 = points[i][1];
             int x2 = points[j][0];
             int y2 = points[j][1];
-            double m = atan2((y1-y2),(x1-x2));
-            mp[m]++;
+            int g = gcd(y1-y2, x1-x2);
+            string dy_dx = to_string((y1-y2)/g) + "_" + to_string((x1-x2)/g);
+            mp[dy_dx]++;
           }
         }
         for(auto [m, cnt]:mp)
