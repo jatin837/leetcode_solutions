d02f3a4a53ab4e7b743bb259d4f6bc3954c25456
jatin.py <pandeyjatin47@gmail.com>
2023-01-16 20:19:45 +0530

    Time: 17 ms (81.26%), Space: 17 MB (78.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    // Returns true if the intervals a and b have a common element.
-    bool doesIntervalsOverlap(vector<int>& a, vector<int>& b) {
-        return min(a[1], b[1]) - max(a[0], b[0]) >= 0;
+  bool overlap(vector<int>&p1, vector<int>&p2){
+    return p2[0] <= p1[1];
+  }
+  vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    int indx = lower_bound(begin(intervals), end(intervals), newInterval) - begin(intervals);
+    intervals.insert(begin(intervals) + indx, newInterval);
+    int b = indx;
+    if(b && (intervals[b][0] <= intervals[b-1][1]))
+      b -= 1;
+    int e = b;
+    vector<int>curr = intervals[b];
+    while(e < intervals.size() && overlap(curr, intervals[e])){
+      curr[0] = min(curr[0], intervals[e][0]);
+      curr[1] = max(curr[1], intervals[e][1]);
+      e++;
     }
-    
-    // Return the interval having all the elements of intervals a and b.
-    vector<int> mergeIntervals(vector<int>& a, vector<int>& b) {
-        return {min(a[0], b[0]), max(a[1], b[1])};
-    }
-
-    // Insert the interval newInterval, into the list interval keeping the sorting order intact.
-    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int index = lower_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
-        
-        if (index != intervals.size()) {
-            intervals.insert(intervals.begin() + index, newInterval);
-        } else {
-            intervals.push_back(newInterval);
-        }
-    }
-    
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // Insert the interval first before merge processing.
-        insertInterval(intervals, newInterval);
-        
-        vector<vector<int>> answer;
-        int i=0; int j=0;
-        while(j < intervals.size() && i <= j){
-            vector<int> currInterval = {intervals[i][0], intervals[i][1]};
-            while (j < intervals.size() && doesIntervalsOverlap(currInterval, intervals[j])) {
-                currInterval = mergeIntervals(currInterval, intervals[j]);
-                j++;
-            }
-            i = j;
-            answer.push_back(currInterval);
-        }
-        
-        return answer;
-    }
+    vector<vector<int>>ret;
+    for(int i=0; i<b; i++)
+      ret.push_back(intervals[i]);
+    ret.push_back(curr);
+    for(int i=e; i<intervals.size(); i++)
+      ret.push_back(intervals[i]);
+    return ret;
+  }
 };
