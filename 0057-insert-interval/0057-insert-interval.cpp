d4bfe7e65fb25cbd4d00e41113753379990f4c87
jatin.py <pandeyjatin47@gmail.com>
2023-01-16 20:29:24 +0530

    Time: 10 ms (97.88%), Space: 17.1 MB (45.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -8,15 +8,16 @@ public:
     intervals.insert(begin(intervals) + indx, newInterval);
     int b = indx - (indx && overlap(intervals[indx-1], intervals[indx]));
     int e = b;
+    vector<vector<int>>ret;
+    for(int i=0; i<b; i++)
+      ret.push_back(intervals[i]);
     vector<int>curr = intervals[b];
     while(e < intervals.size() && overlap(curr, intervals[e])){
       curr[0] = min(curr[0], intervals[e][0]);
       curr[1] = max(curr[1], intervals[e][1]);
       e++;
     }
-    vector<vector<int>>ret;
-    for(int i=0; i<b; i++)
-      ret.push_back(intervals[i]);
+
     ret.push_back(curr);
     for(int i=e; i<intervals.size(); i++)
       ret.push_back(intervals[i]);
