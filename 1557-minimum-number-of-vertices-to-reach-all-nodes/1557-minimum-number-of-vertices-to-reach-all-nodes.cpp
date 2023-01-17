a39b2ca7c0ac6ec45ccf2fda72e2be76dfe64d45
jatin.py <pandeyjatin47@gmail.com>
2023-01-17 21:39:37 +0530

    Time: 350 ms (70.51%), Space: 106.8 MB (38.51%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-  void dfs(int node, vector<vector<int>>&G, vector<bool>&visited){
-    visited[node] = true;
-    for(int c:G[node])
-      dfs(c, G, visited);
-  }
   vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
     vector<int>in(n, 0);
     for(auto e:edges)
