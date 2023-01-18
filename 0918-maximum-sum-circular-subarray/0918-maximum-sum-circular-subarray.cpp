f1a4a0fa9e24e13d3a3f310019b867e4c49b8936
jatin.py <pandeyjatin47@gmail.com>
2023-01-18 13:33:48 +0530

    Time: 67 ms (85.74%), Space: 40 MB (38.25%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -12,17 +12,16 @@ public:
     int b1 = arr[0];
     int s2 = arr[0];
     int b2 = arr[0];
-    int s = accumulate(begin(arr), end(arr), 0);
+    int s = arr[0];
     for(int i=1; i<arr.size(); i++){
       s1 = max(arr[i], s1 + arr[i]);
       b1 = max(s1, b1);
       s2 = min(arr[i], s2 + arr[i]);
       b2 = min(s2, b2);
+      s += arr[i];
     }
     s1=b1;
     s2=b2;
-    if(s2 == s)
-      return s1;
-    return max(s1, s-s2);
+    return (s2==s)?s1:max(s1, s-s2);
   }
 };
