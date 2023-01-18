a2d6522c8a92512e972905afa0f8966a015b92b4
jatin.py <pandeyjatin47@gmail.com>
2023-01-18 13:30:24 +0530

    Time: 60 ms (94.39%), Space: 39.9 MB (77.39%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-  int kadan(vector<int>&arr, bool mx){
-    int sum = arr[0];
-    int best = arr[0];
-    for(int i=1; i<arr.size(); i++){
-      if(mx){
-        sum = max(arr[i], sum + arr[i]);
-        best = max(sum, best);
-      }else{
-        sum = min(arr[i], sum + arr[i]);
-        best = min(sum, best);
-      }
-    }
-    return best;
-  }
-  int maxSubarraySumCircular(vector<int>& nums) {
+
+  int maxSubarraySumCircular(vector<int>& arr) {
     // find the subarray sum which is maximum(s1) and minimum(s2)
     // than, return the max of (s1, s-s2)
     // where s is the sum of the whole array
-    int s1 = kadan(nums, true);
-    int s2 = accumulate(begin(nums), end(nums), 0) - kadan(nums, false);
-    if(s2 == 0)
+    // if s-s2 == 0 => complete array is negetive
+    // in such a case, the algorithm will try to remove all the elements from array
+    // which is not allowed in the problem statement
+    int s1 = arr[0];
+    int b1 = arr[0];
+    int s2 = arr[0];
+    int b2 = arr[0];
+    int s = accumulate(begin(arr), end(arr), 0);
+    for(int i=1; i<arr.size(); i++){
+      s1 = max(arr[i], s1 + arr[i]);
+      b1 = max(s1, b1);
+      s2 = min(arr[i], s2 + arr[i]);
+      b2 = min(s2, b2);
+    }
+    s1=b1;
+    s2=b2;
+    if(s2 == s)
       return s1;
-    return max(s1, s2);
+    return max(s1, s-s2);
   }
 };
