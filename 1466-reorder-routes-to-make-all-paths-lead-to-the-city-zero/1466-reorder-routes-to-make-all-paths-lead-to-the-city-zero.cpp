e5e98c43c3320bf48a40db35fed0e18833f45ae5
jatin.py <pandeyjatin47@gmail.com>
2023-01-20 13:06:42 +0530

    Time: 421 ms (76.17%), Space: 111.2 MB (65.87%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
   int minReorder(int n, vector<vector<int>>& connections) {
-    vector<vector<int>>G(n);
-    set<vector<int>>conn(connections.begin(), connections.end());
+    vector<vector<pair<int, bool>>>G(n);
     for(vector<int>e:connections)
-      G[e[0]].push_back(e[1]), G[e[1]].push_back(e[0]);
+      G[e[0]].push_back({e[1],true}), G[e[1]].push_back({e[0], false});
     int cnt = 0;
     queue<pair<int, int>>q;
     q.push({0, -1});
     while(q.size()){
       auto [curr, parent] = q.front();
       q.pop();
-      for(int c:G[curr]){
+      for(auto [c, conn]:G[curr]){
         if(c != parent){
           q.push({c, curr});
-          if(conn.count({c, curr}) == 0){
-            conn.insert({c,curr});
-            conn.erase({curr,c});
-            cnt++;
-          }
+          cnt += conn;
         }
       }
     }
