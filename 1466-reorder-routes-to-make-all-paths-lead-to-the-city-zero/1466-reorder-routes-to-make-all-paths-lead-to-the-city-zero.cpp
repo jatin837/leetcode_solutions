13bffd1d0bdae744c7a908a14dfe49e71d51f02c
jatin.py <pandeyjatin47@gmail.com>
2023-01-20 13:10:29 +0530

    Time: 418 ms (76.63%), Space: 111.2 MB (65.87%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
   int minReorder(int n, vector<vector<int>>& connections) {
+    
+    // build an undirected graph
+    // maintain a status of connection between nodes
+    // we can use a bool to denote a connection between nodes
     vector<vector<pair<int, bool>>>G(n);
     for(vector<int>e:connections)
-      G[e[0]].push_back({e[1],true}), G[e[1]].push_back({e[0], false});
+      G[e[0]].push_back({e[1],true}),
+        G[e[1]].push_back({e[0], false});
+    
+    // finally, do a bfs and count the number of connection from parent to child
+    // basically, these are the connections we need to invert (child should point to parent)
     int cnt = 0;
     queue<pair<int, int>>q;
     q.push({0, -1});
+    
     while(q.size()){
       auto [curr, parent] = q.front();
       q.pop();
@@ -17,6 +26,7 @@ public:
         }
       }
     }
+    
     return cnt;
   }
 };
