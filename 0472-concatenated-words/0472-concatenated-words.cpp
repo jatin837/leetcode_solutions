3bb4dc63c5808529588009fd9acf34c62ab1d548
jatin.py <pandeyjatin47@gmail.com>
2023-01-27 13:00:50 +0530

    Time: 392 ms (46.56%), Space: 42.8 MB (81.19%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
+  bool isConcatenated(string& s, unordered_set<string>&dict){
+    const int length = s.length();
+    vector<bool> dp(length + 1);
+    dp[0] = true;
+    for (int i = 1; i <= length; ++i)
+      for (int j = (i == length ? 1 : 0); j < i; ++j)
+        if(!dp[i])
+          dp[i] = dp[j] && dict.count(s.substr(j, i - j));
+    return dp[length];
+  }
   vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-    // Put all words in a set
-    unordered_set<string> dictionary(words.begin(), words.end());
-    // This will be our final answer
-    vector<string> answer;
-    // for every word, check if it's a concatented word
-    for (const string& word : words) {
-      const int length = word.length();
-      // to check if current word is concatenated, we use dynamic programming
-      vector<bool> dp(length + 1);
-      // empty string is a concatenated string :-(
-      dp[0] = true;
-      
-      // this is a bit magical, but I'll try to decipher it 
-      for (int i = 1; i <= length; ++i)
-        // okay, j will run for i = 0 or 1 depending on weather i is equal to length or not
-        for (int j = (i == length ? 1 : 0); j < i; ++j)
-          if(!dp[i])
-            dp[i] = dp[j] && dictionary.count(word.substr(j, i - j));
-      
-      // if current word is concatenated, put it in our final answer
-      if (dp[length])
-        answer.push_back(word);
-    }
-    // finally return the answer
-    return answer;
+    unordered_set<string> dict(words.begin(), words.end());
+    vector<string> ret;
+    for (string word : words)
+      if(isConcatenated(word, dict))
+        ret.push_back(word);
+    return ret;
   }
 };
