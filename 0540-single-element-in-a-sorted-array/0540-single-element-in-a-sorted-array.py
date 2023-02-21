55acb1244b8423dc013fee2464d1f833a74248f6
jatin.py <pandeyjatin47@gmail.com>
2023-02-21 15:37:49 +0530

    Time: 181 ms (59.35%), Space: 23.8 MB (39.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -2,9 +2,18 @@ class Solution:
   def singleNonDuplicate(self, nums: List[int]) -> int:
     left, right = 0, len(nums)-1
     while(left < right):
-      print(left, right)
       mid = (left+right)//2
-      if(nums[mid-1] != nums[mid] and nums[mid+1] != nums[mid]):
+      if(nums[mid-1] == nums[mid]):
+        if(mid%2):
+          left = mid+1
+        else:
+          right = mid-1
+      elif(nums[mid+1] == nums[mid]):
+        if(mid%2):
+          right = mid-1
+        else:
+          left = mid+1
+      else:
         return nums[mid]
       ## If mid is odd
       ##    number at mid-1 index should be equal to number at mid
@@ -14,14 +23,14 @@ class Solution:
       ##    number at mid+1 index should be equal to number at mid
       ##    if that's not the case, that means single element must exist on left side of array
       ##    If that's the case, that means single element must exist on right side of array           
-      if(mid%2):
-        if(nums[mid-1] == nums[mid]):
-          left = mid+1
-        else:
-          right = mid-1
-      else:
-        if(nums[mid+1] == nums[mid]):
-          left = mid+1
-        else:
-          right = mid-1
+###   if(mid%2):
+###     if(nums[mid-1] == nums[mid]):
+###       left = mid+1
+###     else:
+###       right = mid-1
+###   else:
+###     if(nums[mid+1] == nums[mid]):
+###       left = mid+1
+###     else:
+###       right = mid-1
     return nums[left]
