2cce19f4ede4019ec2a780ebd402bc374c9745fa
jatin.py <pandeyjatin47@gmail.com>
2023-02-22 10:22:42 +0530

    Time: 889 ms (17.51%), Space: 17 MB (78.06%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,8 +1,8 @@
 class Solution:
   def shipWithinDays(self, weights: List[int], days: int) -> int:
+    ## Given a capacity of conveyor belt and weights, calculate the number of days to ship all package
     def calcDays(ws, cap):
-      cnt = 0
-      tot = 0
+      cnt, tot = 0, 0
       for w in ws:
         if(tot + w <= cap):
           tot += w
@@ -11,8 +11,16 @@ class Solution:
           tot = w
           cnt += 1
       return cnt+1
-    left = max(weights)
-    right = sum(weights)
+    
+    
+    ## Our solution is going to be a weight capacity
+    ## The range of solution lies between [max(weights), sum(weights)]
+    ## Clarification:
+      ## think of what the the minimum capacity should be to ship each weight in len(weights) days
+      ## think of what the the maximum capacity should be to ship all weights in one day
+      ## our weight lies in above range, so the problem boils down to finding a capacity in above range such that weights are shipped in exactly desired days
+      ## Hence, we are using binary search
+    left, right = max(weights), sum(weights)
     while(left <= right):
       mid = (left + right)//2
       if(calcDays(weights, mid) <= days):
