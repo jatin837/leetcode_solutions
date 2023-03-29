f0a2aa0486b9dd64f81c886f111bf0ff3b2902ff
jatin.py <pandeyjatin47@gmail.com>
2023-03-29 11:37:50 +0530

    Time: 44 ms (64.24%), Space: 13.7 MB (100.00%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
       satisfaction.sort()
-      i = len(satisfaction)-2
-      while i >= 0:
-        satisfaction[i] += satisfaction[i+1]
-        i-=1
-      i = len(satisfaction)-2
-      while i >= 0:
-        satisfaction[i] += satisfaction[i+1]
-        i-=1
-      
-      return max(0, max(satisfaction))
+      def suffix_sum(nums):
+        i = len(nums)-2
+        while i >= 0:
+          nums[i] += nums[i+1]
+          i-=1
+        return nums
+      return max(0, max(suffix_sum(suffix_sum(satisfaction))))
       
       
       
